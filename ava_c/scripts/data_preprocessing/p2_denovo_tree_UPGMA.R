e5f10ffc62386d6e594a5b106597558d71ffb34e
Aaron_Y <amyerke@uncc.edu>
2020-12-31 12:52:40 -0500

    Uses "project" variable in file paths. Updated file paths for new project. Commented fitGTR lines as they are computationally heavy.

--- ava_c/scripts/data_preprocessing/p2_denovo_tree_UPGMA.R
+++ ava_c/scripts/data_preprocessing/p2_denovo_tree_UPGMA.R
@@ -1,19 +1,11 @@
 # Author: Aaron Yerke
-# This is a pipeline that was created to explore the philr transform.
+# This is a pipeline that was created to build denovo trees.
 # Many resources were used to create this pipeline:
 #   Processing the sequences through DADA2, making trees with phangorn, and putting them into phyloseq objects:
 #     This is by far the biggest source:
 #     https://github.com/spholmes/F1000_workflow/blob/master/MicrobiomeWorkflow/MicrobiomeWorkflowII.Rmd
 #     or (in article format)
 #     https://f1000research.com/articles/5-1492/v2
-#     
-#   DADA2:
-#     https://benjjneb.github.io/dada2/tutorial.html
-#   
-#   Using philr:
-#     https://bioconductor.org/packages/release/bioc/html/philr.html
-#     Code example:
-#       https://bioconductor.org/packages/release/bioc/vignettes/philr/inst/doc/philr-intro.R
 
 rm(list = ls()) #clear workspace
 
@@ -33,13 +25,11 @@ library("phyloseq")
 
 ##----------------Establish directory layout------------------------##
 home_dir = file.path('~','git','balance_tree_exploration')
+project = "ava_c"
 #home_dir = file.path('cloud','project')
-output_dir = file.path(home_dir, "ava_c", 'output')
-project = "philr_western_gut"
-f_path <- file.path(home_dir, "sequences") # CHANGE ME to the directory containing the fastq files after unzipping.
-# list.files(f_path)
+output_dir = file.path(home_dir, project, 'output')
 
-setwd(file.path(home_dir))
+# setwd(file.path(home_dir))
 
 print("Established directory layout")
 
@@ -63,34 +53,34 @@ phangAlign <- phyDat(as(alignment, "matrix"), type="DNA")
 dm <- dist.ml(phangAlign)#create distance matrix
 treeNJ <- upgma(dm) #make tree
 fit = pml(treeNJ, data=phangAlign)#fit model
-fitGTR <- update(fit, k=4, inv=0.2)#fit model with updated parameters
-fitGTR <- optim.pml(fitGTR, model="GTR", optInv=TRUE, optGamma=TRUE,
-                    rearrangement = "stochastic", control = pml.control(trace = 0))
-print("phangorn completed")
+# fitGTR <- update(fit, k=4, inv=0.2)#fit model with updated parameters
+# fitGTR <- optim.pml(fitGTR, model="GTR", optInv=TRUE, optGamma=TRUE,
+#                     rearrangement = "stochastic", control = pml.control(trace = 0))
+# print("phangorn completed")
 
-myMeta = read.table(file.path(home_dir, "ava_c", "fullMetadata.tsv"), 
+myMeta = read.table(file.path(home_dir, "ava_c", "metadata_SRA.tsv"), 
                     sep="\t", 
                     header=TRUE, 
+                    row.names = "sample_name", 
                     check.names = FALSE,
                     stringsAsFactors=FALSE)
 
 ps <- phyloseq(otu_table(seqtab, taxa_are_rows=FALSE),
                sample_data(myMeta),
                tax_table(taxTab),
-               phy_tree(fitGTR$tree))
+               phy_tree(treeNJ))
 # ps
 print("Created ps")
 
 #examine tree
 library("ape")
 
-pdf(paste0(project,"_2", ".pdf"))
+pdf(file.path(output_dir, "graphics", paste0("upgma_denovo","_2", ".pdf")))
 
 plot_tree(ps, "treeonly", nodeplotblank, ladderize="left")
 
-plot_tree(ps, ladderize="left", color="Religion")
+plot_tree(ps, ladderize="left", color="host_phenotype")
 
 dev.off()
 
-# setwd(file.path(home_dir, "philr_pipelines"))
 saveRDS(ps, file.path(output_dir, "r_objects","denovo_tree_UPGMA_phyloseq_obj.rds"))
