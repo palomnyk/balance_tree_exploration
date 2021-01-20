11e92bf060aacde29033521f8514cfb6edd81eab
Aaron_Y <amyerke@uncc.edu>
2021-01-20 15:29:30 -0500

    Script for building pruned ref trees with some new troubleshooting.

--- r_libraries/cml_scripts/transformations/p2.1_ps_w_ref_tree.R
+++ r_libraries/cml_scripts/transformations/p2.1_ps_w_ref_tree.R
@@ -1,45 +1,80 @@
-# Author: Aaron Yerke
+#!/usr/bin/env Rscript
+# Author: Aaron Yerke (aaronyerke@gmail.com)
 # Convert output from p1_dada2_rd1.R to silva based taxonomy. Loosely following :
 #   http://benjjneb.github.io/dada2/training.html
 
+##-cml argument processing------------------------------------------##
+if (!requireNamespace("optparse", quietly = TRUE)){
+  install.packages("optparse")
+}
+library("optparse")
+
+option_list <- list(
+  optparse::make_option(c("-d", "--homedir"), type="character", 
+                        default=file.path('~','git','balance_tree_exploration'), 
+                        help="dataset dir path", metavar="character"),
+  optparse::make_option(c("-p", "--project"), type="character", default=NULL, 
+                        help="project folder", metavar="character"),
+  optparse::make_option(c("-m", "--metadata"), type="character", default=NULL,
+                        help="metadata file path with filename", metavar="character"),
+  optparse::make_option(c("-l", "--metadata_delim"), type="character", default=NULL,
+                        help="metadata file deliminator", metavar="character"),
+  optparse::make_option(c("-r", "--metadata_rowname"), type="character", default=NULL,
+                        help="metadata file row to use for row names", metavar="character")
+); 
+
+opt_parser <- optparse::OptionParser(option_list=option_list);
+
+opt <- optparse::parse_args(opt_parser);
+
+print(opt)
+
+##-load other dependencies------------------------------------------##
 if (!requireNamespace("BiocManager", quietly = TRUE)){
   install.packages("BiocManager")
   BiocManager::install("dada2",type = "source", checkBuilt = TRUE)
   BiocManager::install("DECIPHER")
 }
 
+library("ape")
 library("DECIPHER")
 library("dada2")
 library("phyloseq")
+print("external libraries loaded")
 
-home_dir = file.path('~','git','balance_tree_exploration')
-#home_dir = file.path('cloud','project')
-project <- "ava_c"
-output_dir <- file.path(home_dir, project, 'output')
+##-Establish directory layout---------------------------------------##
+home_dir <- opt$homedir
+project <- opt$project
+output_dir = file.path(home_dir, project, 'output')
 
-setwd(file.path(home_dir, "ava_c"))
-myMeta = read.table(file.path("metadata_SRA.tsv"), 
-                    sep="\t", 
-                    header=TRUE, 
-                    row.names = "sample_name", 
+##-import tables----------------------------------------------------##
+myMeta = read.table(opt$metadata,
+                    sep=opt$metadata_delim,
+                    header=TRUE,
+                    row.names = opt$metadata_rowname,
                     check.names = FALSE,
                     stringsAsFactors=FALSE)
 
-##---------------------Import R objects-----------------------------##
+print("Imported tables")
+
+
+##-Import R objects and data preprocessing--------------------------##
 con <- gzfile(file.path( output_dir, "r_objects", "ForwardReads_DADA2.rds"))
 seqtab <- readRDS(con)
 close(con)
 
 con <- gzfile(file.path( output_dir, "r_objects", "ForwardReads_DADA2_taxonomy.rds"))
-taxTab <- readRDS(con)
+taxaTab <- readRDS(con)
 close(con)
 
 print("Imported R objects")
 
-tree <- read_tree(file.path(home_dir, "taxonomy" , "silva","viFy10M5J2nvIBpCLM-QMQ_newick.txt"))
+
+##-Build tree------------------------------------------------------##
+tree <- phyloseq::read_tree(file.path(home_dir, "taxonomy" , "silva","viFy10M5J2nvIBpCLM-QMQ_newick.txt"))
 
 pdf(file = file.path(output_dir, "graphics", "silva_ref_tree.pdf"))
-plot_tree(tree, nodelabf=nodeplotblank, label.tips="taxa_names", ladderize="left")
+phyloseq::plot_tree(tree, nodelabf=nodeplotblank, label.tips="taxa_names", ladderize="left")
 dev.off()
 
 tree_key <- read.table(file.path(output_dir, "tree_process_blast", "parsed_output.csv"),
@@ -74,8 +109,6 @@ for (i in 1:length(tree$tip.label)){
       match_df[id,"num_seqs"] = count + 1
       
     }else{
-    
-    
     new_row = data.frame(
       seqs=new_lab,
       num_seqs=1,
@@ -83,8 +116,6 @@ for (i in 1:length(tree$tip.label)){
     row.names(new_row) = c(id)
     match_df = rbind(match_df, new_row)
     }
-    
-    
 
   }else{
     unmatched_ids = c(unmatched_ids, id)
@@ -92,22 +123,39 @@ for (i in 1:length(tree$tip.label)){
   # index = which(tree_key$sseqid == id)
   # print("not in")
 }
+
 print(paste("unmatched ids:", length(unmatched_ids)))
 plot_tree(tree)
 
-print(length(tree$tip.label))
+print(paste("num tree tips pre pruning:", length(tree$tip.label)))
   
-tree = prune_taxa(row.names(tree_key), tree)
+tree <- phyloseq::prune_taxa(row.names(tree_key), tree)
 
-print(length(tree$tip.label))
+print(paste("num tree tip.label post pruning:", length(tree$tip.label)))
 
-pdf(file = file.path(output_dir, "graphics", "ava_c_ref_tree.pdf"))
+pdf(file = file.path(output_dir, "graphics", paste0(project, "_ref_tree.pdf")))
 plot_tree(tree, nodelabf=nodeplotblank, label.tips="taxa_names", ladderize="left")
 dev.off()
 
-ps <- phyloseq(otu_table(seqtab, taxa_are_rows=FALSE),
+print("tree plotted")
+
+print(paste("num duplicated tips:", sum(duplicated(tree$tip.label))))
+
+print(paste("duplicated tips:", tree$tip.label[duplicated(tree$tip.label)]))
+
+if( sum(duplicated(tree$tip.label)) > 0){
+  print("removing duplicated tip")
+  tree <- ape::drop.tip(tree, tree$tip.label[duplicated(tree$tip.label)], trim.internal = TRUE, subtree = FALSE,
+                root.edge = 0, rooted = is.rooted(tree), collapse.singles = TRUE,
+                interactive = FALSE)
+}
+
+print(paste("num duplicated rows taxatab: ", sum(duplicated(row.names(taxaTab)))))
+print(paste("num duplicated cols taxatab: ", sum(duplicated(colnames(taxaTab)))))
+
+ps <- phyloseq::phyloseq(otu_table(seqtab, taxa_are_rows=FALSE),
                sample_data(myMeta),
-               tax_table(taxTab),
+               tax_table(taxaTab),
                phy_tree(tree)
 )
 
