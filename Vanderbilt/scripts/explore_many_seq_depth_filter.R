00f1ea50a5e1ed3cd41a29efe5cfaa57a2bb27ef
Aaron_Y <amyerke@uncc.edu>
2021-03-01 12:19:11 -0500

    progress update

--- Vanderbilt/scripts/explore_many_seq_depth_filter.R
+++ Vanderbilt/scripts/explore_many_seq_depth_filter.R
@@ -5,22 +5,6 @@
 rm(list = ls()) #clear workspace
 
 ##-functions--------------------------------------------------------##
-pca_total_seqs_plot <- function(df, ts, main_tit, mds){
-  #Function for updating 
-  ##-Create PCA-------------------------------------------------------##
-  my_prcmp <- prcomp(df, 
-                     center = TRUE)#,
-  # scale = TRUE)
-  ##-Extract PCA matrix and convert to dataframe----------------------##
-  myPCA <- data.frame(my_prcmp$x)
-  my_spear <- cor.test(log10(ts), myPCA[,mds],
-                       method = "kendall")
-  plot(log10(ts), myPCA[,2],
-       main = paste0( main_tit, " PCA", mds,"\nr_sq: ", my_spear$estimate^2),
-       sub = paste0("Kend: ", my_spear$estimate, " Kend^2: ", ),
-       )
-  return(my_spear$estimate)
-}
 munge_ref_ps <- function(ps){
   #ps must be a phyloseq object
   #requires phyloseq and ape packages to be loaded in the env
@@ -32,9 +16,9 @@ munge_ref_ps <- function(ps){
 }
 
 ##-Load Depencencies------------------------------------------------##
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("ALDEx2")
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+if (!requireNamespace("ALDEx2", quietly = TRUE)) BiocManager::install("ALDEx2")
+if (!requireNamespace("edgeR", quietly = TRUE)) BiocManager::install("edgeR")
 library("compositions")
 library("phyloseq")
 library("vegan")
@@ -43,6 +27,7 @@ library("philr")
 library("ape")
 library("ALDEx2")
 library("ggplot2")
+library("edgeR")
 
 ##-Establish directory layout---------------------------------------##
 home_dir <- file.path('~','git','balance_tree_exploration')
@@ -71,7 +56,7 @@ metadata <- read.table(file.path(home_dir, project, "patient_metadata.tsv"),
 #Plot shannon diversity against log10(total_seqs)
 #Plot other normalization methods: otu log 100 and alr and clr 
 # min_seq_depths <- c(0,100,500,1000,2000,5000,10000,50000)
-my_ds_names <- c( "raw seqs", "clr(raw seqs)", "lognorm raw seqs", "philr ref", "DESeq2")
+my_ds_names <- c( "raw seqs", "clr(raw seqs)", "lognorm raw seqs", "philr ref", "DESeq2", "ALDEx2.clr")
 min_seq_depths <- c(0, 500, 1000, 5000, 10000, 50000)
 mds_depth <- 5
 
@@ -88,33 +73,33 @@ var_exp <- vector(mode = "integer", length = length(my_ds_names) * length(min_se
 spear_cor <- vector(mode = "integer", length = length(my_ds_names) * length(min_seq_depths) * mds_depth)
 
 counter <- 1
-pdf(file = file.path(output_dir, "graphics", "explore_seq_depth_artifact_var_seq_depth_PCA12345_4ds.pdf"))
 for(s in 1:length(min_seq_depths)){
   seq_d <- min_seq_depths[s]#new sequencing depth
   sd_filt_asv <- asv_table[total_seqs$total_seqs >= seq_d,]#dataset 1
   print(paste("sd_filtered dim:", paste(dim(sd_filt_asv))))
-  safe_rns <- intersect(row.names(ref_ps@otu_table), row.names(sd_filt_asv))
-  my_clr <- compositions::clr(sd_filt_asv)#dataset 3
+  safe_rns <- intersect(row.names(ref_ps@otu_table), row.names(sd_filt_asv)) #rows for this iterate
+  my_clr <- compositions::clr(sd_filt_asv)#dataset 2
   new_tree <- phyloseq::prune_taxa(colnames(sd_filt_asv), ref_ps@phy_tree)#update tree for new phyloseq obj
-  new_ref_ps <- prune_samples(safe_rns, ref_ps)
+  new_ref_ps <- prune_samples(safe_rns, ref_ps) #remove non-safe rows from ps
   new_ref_ps <- munge_ref_ps(new_ref_ps)
+  print(paste("new dim ref ps:", dim(data.frame(new_ref_ps@otu_table))))
+  #create DESeq2 dtaset from new ref ps
   new_DESeq2 <- phyloseq::phyloseq_to_deseq2(new_ref_ps, design= ~ 1)#dataset 5
   new_DESeq2 <- estimateSizeFactors(new_DESeq2)
   new_DESeq2 <- t(counts(new_DESeq2, normalized=T))
   print(paste("new DSeq:", paste(dim(new_DESeq2))))
   
-  print(paste("new dim ref ps:", dim(data.frame(new_ref_ps@otu_table))))
-  print("made new ps")
-  ln_ps <- lognorm(new_ref_ps@otu_table)#dataset 7
-
   ref_philr <- philr::philr(new_ref_ps@otu_table, new_ref_ps@phy_tree,
                             part.weights='enorm.x.gm.counts',
                             ilr.weights='blw.sqrt')#dataset 4
   print(paste("made new philr", dim(as.data.frame(ref_philr))))
-  # print(any(is.na(data.frame(ref_philr))))
 
   ln_asv <- lognorm(sd_filt_asv)#dataset 6
-  my_datasets <- list(sd_filt_asv, my_clr,  ln_asv, ref_philr, new_DESeq2)
+  # ald <- aldex.clr(sd_filt_asv, mc.samples=150, denom="all", verbose=F)
+  
+  ald <- aldex.clr(t(sd_filt_asv), conds = metadata$Genotype[safe_rns], mc.samples=150, denom="all", verbose=F)
+  
+  my_datasets <- list(sd_filt_asv, my_clr,  ln_asv, ref_philr, new_DESeq2, ald)
   
   for( ds in 1:length(my_datasets)){
     print(my_ds_names[ds])
@@ -127,12 +112,6 @@ for(s in 1:length(min_seq_depths)){
     myPCA <- data.frame(my_prcmp$x)
     my_var_exp <- my_prcmp$sdev^2/sum(my_prcmp$sdev^2)
     
-    # if ( s == 1){
-    #   my_cor <- cor(log10(total_seqs[total_seqs >= seq_d]), myPCA[,1], method = "spearman")
-    #   plot(myPCA[,1], log10(total_seqs[total_seqs >= seq_d]),
-    #        main = paste(my_ds_names[ds], " vs PCA1"),
-    #        sub = paste0("Spearman: ", my_cor$estimate))
-    # }
     for (md in 1:mds_depth){
       # kend[counter] <- cor.test(log10(total_seqs[total_seqs > seq_d]), myPCA[,md], method = "kendall")$estimate
       # perma_r2[counter] <- adonis2(log10(total_seqs[total_seqs > seq_d]) ~ myPCA[,md])$R2[1]
@@ -141,19 +120,13 @@ for(s in 1:length(min_seq_depths)){
       mds_lev[counter] <- md
       seq_depth[counter] <- seq_d
       var_exp[counter] <- my_var_exp[md]
-      spear_cor[counter] <- cor(log10(total_seqs[total_seqs >= seq_d]), myPCA[,md], method = "spearman")
+      spear_cor[counter] <- cor(total_seqs[total_seqs >= seq_d], myPCA[,md], method = "spearman")
       counter <- counter + 1
     }
   }
 }
 
 result_df <- data.frame(ds_num, ds_nam, perma_r2, mds_lev, seq_depth, var_exp, spear_cor)
-g <- ggplot(result_df, aes(x = as.factor(seq_depth), y = perma_r2)) +
-  geom_text(aes(label=mds_lev, color = factor(ds_nam))) +
-  ggtitle("Permanova R^2 filtered seq depths")
-  # scale_x_discrete(limits=c("2","1","0.5"))
-g
-dev.off()
 
 for (i in 1:max(result_df$mds_lev)){
   pca_only <- result_df[mds_lev == i, ]
@@ -162,14 +135,12 @@ for (i in 1:max(result_df$mds_lev)){
     geom_bar(width = 0.8, position=position_dodge(width = 1), stat="identity",) +
     ggtitle(paste0(project, ": PCA",  i, " vs total sequences per sample")) +
     xlab("Min sequence depth per sample") +
-    ylab("Spearman Rsq") +
-    labs(fill = "Transformations")
+    ylab("Spearman Rsq") 
     # geom_text(aes(x=as.factor(seq_depth), y=spear_cor^2, 
     #               label = round(var_exp*100, digits = 0)),
     #           position = position_dodge(width = 1))
-  # ylab(y_lab)
   ggsave(
-    filename = file.path(output_dir, "graphics", paste0(project, "_PCA",  i, "_5ds_var_exp.pdf")),
+    filename = file.path(output_dir, "graphics", paste0(project, "_PCA",  i, "bar_5ds_var_exp.pdf")),
     plot = g,
     device = "pdf"
   )
@@ -185,21 +156,16 @@ for (i in 1:max(result_df$mds_lev)){
     ggtitle(paste0(project, ": PCA",  i, " vs total sequences per sample")) +
     xlab("Min sequence depth per sample") +
     ylab("Spearman Rsq") + 
-    labs(fill = "Transformations") +
+    theme(legend.title =  "Transformations") +
     theme_minimal()
   g
   print(g)
-  # ggsave(
-  #   filename = file.path(output_dir, "graphics", paste0(project, "_PCA",  i, "_5ds_var_exp_points.pdf")),
-  #   plot = g,
-  #   device = "pdf"
-  # )
 }
 dev.off()
 
 
 
-g <- aldex.clr(asv_table, mc.samples=150, denom="all", verbose=F)
+g <- aldex.clr(t(asv_table), conds = metadata$Genotype[row.names(asv_table)], mc.samples=150, denom="all", verbose=F)
 
 # conda create --name R35 R=3.5 
 
