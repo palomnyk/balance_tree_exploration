4a0cc0237308f45988fbd7ed691474d07b3750e1
Aaron_Y <amyerke@uncc.edu>
2021-03-17 14:31:02 -0400

    Corrected metavar in optparse options. Stuck reverse read portion in try/catch. Added messages updates of script progress.

--- r_libraries/cml_scripts/data_preprocessing/p0_dada2_find_trunLen.R
+++ r_libraries/cml_scripts/data_preprocessing/p0_dada2_find_trunLen.R
@@ -14,13 +14,13 @@ library("optparse")
 option_list <- list(
   make_option(c("-d", "--homedir"), type="character", 
               default=file.path('~','git','balance_tree_exploration'), 
-              help="dataset dir path", metavar="character"),
+              help="dataset dir path", metavar="home dir"),
   make_option(c("-p", "--project"), type="character", default=NULL, 
-              help="project folder", metavar="character"),
+              help="project folder", metavar="project"),
   make_option(c("-f", "--r1_pattern"), type="character", default="_R1.fastq.gz", 
-              help="pattern for forward filenames for paired-end reads", metavar="character"),
+              help="pattern for forward filenames for paired-end reads", metavar="forward read suffix"),
   make_option(c("-r", "--r2_pattern"), type="character", default="_R2.fastq.gz", 
-              help="pattern for reverse filenames for paired-end reads", metavar="character")
+              help="pattern for reverse filenames for paired-end reads", metavar="reverse read suffix")
 ); 
 
 opt_parser <- OptionParser(option_list=option_list);
@@ -40,20 +40,33 @@ f_path <- file.path(home_dir, project, "downloaded_seqs") # CHANGE ME to the dir
 
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
 fnFs <- sort(list.files(f_path, pattern=opt$r1_pattern, full.names = TRUE))
-fnRs <- sort(list.files(f_path, pattern=opt$r2_pattern, full.names = TRUE))
 # Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
 sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
-
 plotQualF <- plotQualityProfile(fnFs[1:2])
+message("created forward plotQualityProfile")
 
-plotQualR <- plotQualityProfile(fnRs[1:2])
 dir.create(file.path(output_dir, "graphics"), showWarnings = FALSE)
 setwd(file.path(output_dir, "graphics"))
 
 png(filename="plotQualF.png")
 plot(plotQualF)
 dev.off()
+message("saved forward plotQualityProfile")
 
-png(filename="plotQualR.png")
-plot(plotQualR)
-dev.off()
+tryCatch({
+  fnRs <- sort(list.files(f_path, pattern=opt$r2_pattern, full.names = TRUE))
+  plotQualR <- plotQualityProfile(fnRs[1:2])
+  message("Created reverse plotQualityProfile")
+  png(filename="plotQualR.png")
+  plot(plotQualR)
+  dev.off()
+  message("Saved reverse plotQualityProfile")
+}, warning = function(w) {
+  print('Opps, a warning is thrown')
+  message(w)
+}, error = function(e) {
+  print('Opps, an error is thrown')
+  message(cond)
+})
+
+message("Reached end of script")
