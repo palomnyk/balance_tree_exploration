efdc1ef902af1058b5c9daecb05ec3370ddde9f0
Aaron_Y <amyerke@uncc.edu>
2021-09-23 11:22:41 -0400

    Script overhaul.

--- Vanderbilt/scripts/shannon_diversity/shannon_diversity_seq_depth.R
+++ Vanderbilt/scripts/shannon_diversity/shannon_diversity_seq_depth.R
@@ -1,31 +1,15 @@
 # Author: Aaron Yerke (aaronyerke@gmail.com)
-# Script for testing each transformation against different sequence depth to find the best one
+# Script for Shannon sequence depth vs metadata
 # This was helepful: https://github.com/jsilve24/philr/blob/master/vignettes/philr-intro.Rmd#L142
 
 rm(list = ls()) #clear workspace
 
 ##-functions--------------------------------------------------------##
-munge_ref_ps <- function(ps){
-  #ps must be a phyloseq object
-  #requires phyloseq and ape packages to be loaded in the env
-  # ps <- filter_taxa(ps, function(x) sum(x > 3) > (0.2*length(x)), TRUE)
-  # ps <- filter_taxa(ps, function(x) sd(x)/mean(x) > 3.0, TRUE)
-  ps <- transform_sample_counts(ps, function(x) x+1)
-  phy_tree(ps) <- makeNodeLabel(phy_tree(ps), method="number", prefix='n')
-  return(ps)
-}
 
 ##-Load Depencencies------------------------------------------------##
 if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-if (!requireNamespace("ALDEx2", quietly = TRUE)) BiocManager::install("ALDEx2")
 if (!requireNamespace("reshape2", quietly = TRUE)) BiocManager::install("reshape2")
-library("compositions")
-library("phyloseq")
 library("vegan")
-library("DESeq2")
-library("philr")
-library("ape")
-library("ALDEx2")
 library("ggplot2")
 library("reshape2")
 
@@ -42,10 +26,6 @@ source(file.path(home_dir, "r_libraries", "table_manipulations.R"))
 
 ##-Import tables and data preprocessing-----------------------------##
 asv_table <- readRDS(file.path(output_dir, "r_objects", "ForwardReads_DADA2.rds"))
-
-ref_ps <- readRDS(file.path(output_dir, "r_objects", "ref_tree_phyloseq_obj.rds"))
-asv_tax <- readRDS(file.path(output_dir, "r_objects", "ForwardReads_DADA2_taxonomy.rds"))
-
 metadata <- read.table(file.path(home_dir, project, "patient_metadata.tsv"), 
                        sep="\t", 
                        header=TRUE, 
@@ -54,17 +34,17 @@ metadata <- read.table(file.path(home_dir, project, "patient_metadata.tsv"),
                        stringsAsFactors=TRUE)
 metadata <- metadata[row.names(metadata) %in% row.names(asv_table), ]
 metadata$type <- droplevels(metadata$type)
+metadata <- metadata[ lapply( metadata, function(x) sum(is.na(x)) / length(x)) < 0.05 ]
+metadata <- metadata[ lapply( metadata, function(x) (length(unique(x)))) > 1 ]
+metadata <- metadata[row.names(metadata) %in% row.names(asv_table), ]
 metadata$type <- factor(metadata$type)
 
-metad_cols <- 1:2
+metad_cols <- which(unlist(lapply(metadata, is.numeric)))
 
 #Plot shannon diversity against log10(total_seqs)
 #Plot other normalization methods: otu log 100 and alr and clr 
 min_seq_depths <- c(0, 500, 1000, 5000, 10000, 20000, 40000)
 mds_depth <- 5
-
-total_seqs <- rowSums(asv_table)
-total_seqs <- data.frame(total_seqs, row.names = row.names(asv_table))
 
 kend <- vector(mode = "numeric", length = length(min_seq_depths) * mds_depth)
 perma_r2 <- vector(mode = "numeric", length = length(min_seq_depths) * mds_depth)
@@ -76,22 +56,18 @@ samples_left <- vector(mode = "integer", length = length(min_seq_depths) * mds_d
 taxa_left <- vector(mode = "integer", length = length(min_seq_depths) * mds_depth)
 zero_count <- vector(mode = "integer", length = length(min_seq_depths) * mds_depth)
 
-meta_cor <- list()
-for (i in metad_cols){
-  meta_cor[[i]] <- vector(mode = "integer", length = length(min_seq_depths) * mds_depth)
-  names(meta_cor)[i] <- colnames(metadata)[i]
-}
-meta_cor <- data.frame(meta_cor)
+meta_cor <- data.frame(matrix(ncol = length(metad_cols), nrow = length(min_seq_depths) * mds_depth))
+colnames(meta_cor) <- names(metad_cols)
 
-# pdf(file = file.path(output_dir, "graphics", "seq_depth_artifact_PCA12345_scatter.pdf"))
+meta_p <- data.frame(matrix(ncol = length(metad_cols), nrow = length(min_seq_depths) * mds_depth))
+colnames(meta_p) <- names(metad_cols)
+
 counter <- 1
 for(s in 1:length(min_seq_depths)){
   seq_d <- min_seq_depths[s]#new sequencing depth
   sd_filt_asv <- asv_table[total_seqs$total_seqs >= seq_d,]#dataset 1
-  print(paste("sd_filtered dim:", paste(dim(sd_filt_asv))))
-  safe_rns <- intersect(row.names(ref_ps@otu_table), row.names(sd_filt_asv)) #rows for this iterate
-  shan_dirv <- vegan::diversity(sd_filt_asv[safe_rns,])
-  
+  cat(paste("sd_filtered dim:", dim(sd_filt_asv)))
+  shan_div <- vegan::diversity(sd_filt_asv)
   print(paste("finished seq depth filter:", s))
   
   ##-Create a PCA-----------------------------------------------------##
@@ -103,38 +79,40 @@ for(s in 1:length(min_seq_depths)){
   myPCA <- data.frame(my_prcmp$x)
   my_var_exp <- my_prcmp$sdev^2/sum(my_prcmp$sdev^2)
   shan_div <- vegan::diversity(sd_filt_asv)
-  for (md in 1:mds_depth){
-    for (m in 1:length(metad_cols)){
-      print(m)
-      meta_cor[counter, m] <- cor.test(shan_div, metadata[safe_rns,m],
-                                       method = "spearman")$estimate
+  if(nrow(sd_filt_asv) > 3){
+    for (md in 1:mds_depth){
+      for (m in 1:length(metad_cols)){
+        my_cor <- cor.test(shan_div, metadata[row.names(sd_filt_asv),metad_cols[m]],
+                           method = "spearman")
+        meta_cor[counter, names(metad_cols)[m]] <- my_cor$estimate
+        meta_p[counter, names(metad_cols)[m]] <- my_cor$p.value
+      }
+      mds_lev[counter] <- md
+      seq_depth[counter] <- seq_d
+      var_exp[counter] <- my_var_exp[md]
+      PCAi_shannon_div_spear_cor[counter] <- cor(shan_div, myPCA[,md], method = "spearman")
+      samples_left[counter] <- nrow(sd_filt_asv)
+      taxa_left[counter] <- sum(colSums(sd_filt_asv) > 0)
+      zero_count[counter] <- sum(sd_filt_asv == 0)
+      counter <- counter + 1
     }
-    mds_lev[counter] <- md
-    seq_depth[counter] <- seq_d
-    var_exp[counter] <- my_var_exp[md]
-    PCAi_shannon_div_spear_cor[counter] <- cor(shan_dirv, myPCA[,md], method = "spearman")
-    samples_left[counter] <- nrow(sd_filt_asv)
-    taxa_left[counter] <- sum(colSums(sd_filt_asv) > 0)
-    zero_count[counter] <- sum(sd_filt_asv == 0)
-    counter <- counter + 1
-  }
+  }#if statement
 }
-# dev.off()
 result_df <- data.frame( mds_lev, seq_depth, var_exp, PCAi_shannon_div_spear_cor, samples_left, zero_count, taxa_left)
 print("created resulting DF")
 
-write.table(result_df, 
-            file = file.path(output_dir, "tables", paste0(project, "_PCA_shandiv_filt_results.csv")),
+comb_result_df <- cbind(result_df, meta_cor)
+
+write.table(comb_result_df, 
+            file = file.path(output_dir, "tables", paste0(project, "_PCA_shandiv_filt_results_cor.csv")),
             sep = ",")
 
-result_df <- read.table(file = file.path(output_dir, "tables", paste0(project, "_PCA_shandiv_filt_results.csv")),
-                        sep = ",")
+comb_result_df <- read.table(file = file.path(output_dir, "tables", paste0(project, "_PCA_shandiv_filt_results_cor.csv")),
+                             sep = ",")
 
-result_df <- cbind(result_df, meta_cor)
-
-melted_results <- reshape2::melt(result_df,
-                       variable.name = "Metadata",
-                       measure.vars = c(names(meta_cor)[metad_cols], "PCAi_shannon_div_spear_cor"))
+melted_results <- reshape2::melt(comb_result_df,
+                                 variable.name = "Metadata",
+                                 measure.vars = c(colnames(meta_cor), "PCAi_shannon_div_spear_cor"))
 
 pdf(file = file.path(output_dir, "graphics", "shan_div_cor_PCA12345_line.pdf"))
 for (i in 1:max(result_df$mds_lev)){
@@ -142,7 +120,7 @@ for (i in 1:max(result_df$mds_lev)){
                        aes(x=seq_depth, y=value^2, group = Metadata)) +
     ggplot2::geom_point(aes(color = factor(Metadata))) +
     ggplot2::geom_line(aes(color = factor(Metadata))) +
-    ggplot2::ggtitle(paste0("PCA",i," Seq depth vs Rsqd of Shannon Diversity vs metadata")) +
+    ggplot2::ggtitle(paste0(project, ", PCA",i," Seq depth vs Rsqd of Shannon Diversity vs metadata")) +
     ggplot2::xlab("Min sequence depth per sample") +
     ggplot2::ylab("Spearman corr") +
     ggplot2::labs(fill = "Metadata") +
@@ -153,4 +131,54 @@ for (i in 1:max(result_df$mds_lev)){
 }
 dev.off()
 
+comb_result_df <- cbind(result_df, meta_p)
+write.table(comb_result_df, 
+            file = file.path(output_dir, "tables", paste0(project, "_PCA_shandiv_filt_results_p.csv")),
+            sep = ",")
+
+comb_result_df <- read.table(file = file.path(output_dir, "tables", paste0(project, "_PCA_shandiv_filt_results_p.csv")),
+                             sep = ",")
+
+melted_results <- reshape2::melt(comb_result_df,
+                                 variable.name = "Metadata",
+                                 measure.vars = c(colnames(meta_cor)))
+
+
+pdf(file = file.path(output_dir, "graphics", "shan_div_p_PCA12345_line.pdf"))
+g <- ggplot2::ggplot(melted_results,
+                     aes(x=seq_depth, y=value, group = Metadata)) +
+  ggplot2::geom_point(aes(color = factor(Metadata))) +
+  ggplot2::geom_line(aes(color = factor(Metadata))) +
+  ggplot2::ggtitle(paste0(project, ", PCA",i," Read depth vs P-val of Shannon Diversity vs metadata")) +
+  ggplot2::xlab("Min sequence depth per sample") +
+  ggplot2::ylab("Pvale") +
+  ggplot2::labs(fill = "Metadata") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  ggplot2::theme_minimal()
+print(g)
+
+g <- ggplot2::ggplot(melted_results,
+                     aes(x=seq_depth, y=log10(value), group = Metadata)) +
+  ggplot2::geom_point(aes(color = factor(Metadata))) +
+  ggplot2::geom_line(aes(color = factor(Metadata))) +
+  ggplot2::ggtitle(paste0(project, ", PCA: ",i," Read depth vs log20(P-val) of Shannon Diversity vs metadata")) +
+  ggplot2::xlab("Min sequence depth per sample") +
+  ggplot2::ylab("log10(pval)") +
+  ggplot2::labs(fill = "Metadata") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  ggplot2::theme_minimal()
+print(g)
+
+g <- ggplot2::ggplot(result_df,
+                     aes(x=seq_depth, y=samples_left)) +
+  ggplot2::geom_point() +
+  ggplot2::geom_line() +
+  ggplot2::ggtitle(paste0(project, ": Number samples vs read depth filt")) +
+  ggplot2::xlab("Min sequence depth per sample") +
+  ggplot2::ylab("Num samples") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  ggplot2::theme_minimal()
+print(g)
+
+dev.off()
 
