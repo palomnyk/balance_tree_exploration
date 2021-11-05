f328a399703a4ef8835189573610addd5591ab9b
Aaron_Y <amyerke@uncc.edu>
2021-11-05 19:39:03 -0400

    Added a bunch of code to verify the low pvals. We still don't understand why they are so low for the metadata$type

--- Vanderbilt/scripts/philr_tree_comparison/pval_vs_pval_plots.R
+++ Vanderbilt/scripts/philr_tree_comparison/pval_vs_pval_plots.R
@@ -5,6 +5,7 @@ rm(list = ls()) #clear workspace
 ##-functions--------------------------------------------------------##
 tree_philr_node_pvals <- function(philr_df, phylo_tree) {
   #function for walking down nodes of a tree and comparing their child nodes
+  #don't need this function anymore
   #internal functions:
   select.tip.or.node <- function(element, tree) {
     # https://stackoverflow.com/questions/51696837/r-phylo-object-how-to-connect-node-label-and-node-number
@@ -41,6 +42,23 @@ tree_philr_node_pvals <- function(philr_df, phylo_tree) {
   return(pvals)
 }
 
+log10_pval_pval_plot <- function(pvals_1, label_1, pvals_2, label_2, meta_c) {
+  # function for making pval plots
+  max_len <- min(length(pvals_1), length(pvals_2))
+  plot_data <- data.frame(pval1 = sort(log10(sample(pvals_1, max_len, replace = F))), 
+                          pval2 = sort(log10(sample(pvals_2, max_len, replace = F))))
+  my_cor <- cor(plot_data$pval1, plot_data$pval2)
+  g <- ggplot2::ggplot(data = plot_data, aes(x = pval1, y = pval2)) +  
+    ggplot2::ggtitle(paste("log10(pval) v  log10(pval)", "Rsq:", round(my_cor, 4),"meta:", meta_c)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_abline(intercept = 0, slope = 1) +
+    ggplot2::theme_classic() +
+    ggplot2::ylab(label_1) +
+    ggplot2::xlab(label_2) +
+    ggplot2::labs(color = "Part weight")
+  return(g)
+}
+
 ##-Load Depencencies------------------------------------------------##
 # if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 # if (!requireNamespace("ape", quietly = TRUE)) BiocManager::install("ape")
@@ -70,6 +88,16 @@ num_cycles <- 20
 if(num_cycles < 3) stop("num_cycles should be 3 or more")
 
 ##-Import tables and data preprocessing-----------------------------##
+print("Loading raw dada2 count table")
+asv_table <- asv_table <- data.frame(readRDS(file.path(output_dir, "r_objects", "ForwardReads_DADA2.rds")))
+
+print("Cleaning Ref tree otu with philr tutorial normalization")
+ref_ps <- readRDS(file.path(output_dir, "r_objects", "ref_tree_phyloseq_obj.rds"))
+
+clean_otu <- data.frame(ref_ps@otu_table@.Data)
+clean_otu <- philr_tutorial_normalization(clean_otu)
+print(paste("nrow orginal ref:", nrow(ref_ps@otu_table), "nrow clean ref: ", nrow(clean_otu)))
+
 print("loading and munging metadata")
 metadata <- read.table(file.path(home_dir, project, "patient_metadata.tsv"), 
                        sep="\t", 
@@ -81,16 +109,7 @@ metadata <- metadata[row.names(metadata) %in% row.names(clean_otu), ]
 metadata$type <- droplevels(metadata$type)
 metadata$type <- factor(metadata$type)
 
-print("Loading raw dada2 count table")
-asv_table <- asv_table <- data.frame(readRDS(file.path(output_dir, "r_objects", "ForwardReads_DADA2.rds")))
-
-print("Cleaning Ref tree otu with philr tutorial normalization")
-ref_ps <- readRDS(file.path(output_dir, "r_objects", "ref_tree_phyloseq_obj.rds"))
-
-clean_otu <- data.frame(ref_ps@otu_table@.Data)
-clean_otu <- philr_tutorial_normalization(clean_otu)
-print(paste("nrow orginal ref:", nrow(ref_ps@otu_table), "nrow clean ref: ", nrow(clean_otu)))
-
+print("'Cleaning' ref_ps")
 phy_tree(ref_ps) <- ape::makeNodeLabel(phy_tree(ref_ps), method="number", prefix='n')
 ref_ps_clean <- phyloseq::phyloseq( otu_table(clean_otu, taxa_are_rows = F), 
                                     phy_tree(ref_ps@phy_tree),
@@ -111,24 +130,22 @@ phy_tree(cln_denovo_tree_ps) <- makeNodeLabel(phy_tree(cln_denovo_tree_ps), meth
 
 ##-Random num seed--------------------------------------------------##
 set.seed(36)
-print("making random trees")
-ref_rand_list <- list()
+print("making orig ref random trees")
+orig_ref_rand_list <- list()
 for (rand in 1:10){
-  rand_tree <- ape::rtree(n = length(ref_ps@phy_tree$tip.label), tip.label = ref_ps@phy_tree$tip.label)
+  rand_tree <- rtree(n = length(ref_ps@phy_tree$tip.label), tip.label = ref_ps@phy_tree$tip.label)
   #put int in philr
   rand_tree_ps <- phyloseq::phyloseq( otu_table(clean_otu, taxa_are_rows = F),
                                       phy_tree(rand_tree),
                                       tax_table(ref_ps@tax_table),
                                       sample_data(ref_ps@sam_data))
   phy_tree(rand_tree_ps) <- ape::makeNodeLabel(phy_tree(rand_tree_ps), method="number", prefix='n')
-  ref_rand_list[[rand]] <- rand_tree_ps
-  names(ref_rand_list)[rand] <- paste0("ref_rand", rand)
+  orig_ref_rand_list[[rand]] <- rand_tree_ps
 }
-
 print("make random trees for cln upgma taxa")
 cln_upgma_rand_list <- list()
 for (rand in 1:10){
-  rand_tree <- ape::rtree(n = length(cln_denovo_tree_ps@phy_tree$tip.label), tip.label = cln_denovo_tree_ps@phy_tree$tip.label)
+  rand_tree <- rtree(n = length(cln_denovo_tree_ps@phy_tree$tip.label), tip.label = cln_denovo_tree_ps@phy_tree$tip.label)
   #put int in philr
   rand_tree_ps <- phyloseq::phyloseq( otu_table(cln_denovo_tree_ps, taxa_are_rows = F),
                                       phy_tree(rand_tree),
@@ -136,22 +153,34 @@ for (rand in 1:10){
                                       sample_data(cln_denovo_tree_ps@sam_data))
   phy_tree(rand_tree_ps) <- ape::makeNodeLabel(phy_tree(rand_tree_ps), method="number", prefix='n')
   cln_upgma_rand_list[[rand]] <- rand_tree_ps
-  names(cln_upgma_rand_list)[rand] <- paste0("cln_upgma_rand", rand)
 }
 
-print("make random trees for original upgma taxa")
-orig_upgma_rand_list <- list()
+print("make random trees for clean ref taxa")
+cln_ref_rand_list <- list()
 for (rand in 1:10){
-  rand_tree <- ape::rtree(n = length(denovo_tree_ps@phy_tree$tip.label), tip.label = denovo_tree_ps@phy_tree$tip.label)
+  rand_tree <- rtree(n = length(ref_ps_clean@phy_tree$tip.label), tip.label = denovo_tree_ps@phy_tree$tip.label)
   #put int in philr
-  rand_tree_ps <- phyloseq::phyloseq( otu_table(denovo_tree_ps, taxa_are_rows = F),
-                                      phy_tree(rand_tree),
-                                      tax_table(denovo_tree_ps@tax_table),
-                                      sample_data(denovo_tree_ps@sam_data))
+  rand_tree_ps <- phyloseq::phyloseq(otu_table(ref_ps_clean, taxa_are_rows = F),
+                                     phy_tree(rand_tree),
+                                     tax_table(ref_ps_clean@tax_table),
+                                     sample_data(ref_ps_clean@sam_data))
   phy_tree(rand_tree_ps) <- ape::makeNodeLabel(phy_tree(rand_tree_ps), method="number", prefix='n')
-  orig_upgma_rand_list[[rand]] <- rand_tree_ps
-  names(orig_upgma_rand_list)[rand] <- paste0("orig_upgma_rand", rand)
+  cln_ref_rand_list[[rand]] <- rand_tree_ps
 }
+
+# print("make random trees for original upgma taxa")
+# orig_upgma_rand_list <- list()
+# for (rand in 1:10){
+#   rand_tree <- ape::rtree(n = length(denovo_tree_ps@phy_tree$tip.label), tip.label = denovo_tree_ps@phy_tree$tip.label)
+#   #put int in philr
+#   rand_tree_ps <- phyloseq::phyloseq( otu_table(denovo_tree_ps, taxa_are_rows = F),
+#                                       phy_tree(rand_tree),
+#                                       tax_table(denovo_tree_ps@tax_table),
+#                                       sample_data(denovo_tree_ps@sam_data))
+#   phy_tree(rand_tree_ps) <- ape::makeNodeLabel(phy_tree(rand_tree_ps), method="number", prefix='n')
+#   orig_upgma_rand_list[[rand]] <- rand_tree_ps
+#   names(orig_upgma_rand_list)[rand] <- paste0("orig_upgma_rand", rand)
+# }
 ##-Calculate pvalues------------------------------------------------##
 print("making philr tables")
 
@@ -159,101 +188,196 @@ print("making philr tables")
 philr_taxa_weights <- c("uniform","gm.counts","anorm","anorm.x.gm.counts","enorm","enorm.x.gm.counts")
 philr_ilr_weights <- c("uniform","blw","blw.sqrt","mean.descendants")
 
+metad_col <- metadata$Age
+
 ref_ps_clean_philr <- philr::philr(df = ref_ps_clean@otu_table,
                          tree = ref_ps_clean@phy_tree)
-ref_ps_clean_pvals <- tree_philr_node_pvals(ref_ps_clean_philr, ref_ps_clean@phy_tree)
+ref_ps_clean_pvals <- apply(ref_ps_clean_philr, 2, function(x) {t.test(x, metad_col)$p.value})
+
+test_pvals <- c()
+
+for (i in 1:ncol())
 
 cln_denovo_philr <- philr::philr(df = cln_denovo_tree_ps@otu_table,
                                    tree = cln_denovo_tree_ps@phy_tree)
-cln_denovo_pvals <- tree_philr_node_pvals(cln_denovo_philr, cln_denovo_tree_ps@phy_tree)
-
-# orig_denovo_philr <- philr::philr(df = denovo_tree_ps@otu_table,
-#                                  tree = denovo_tree_ps@phy_tree)
-# orig_denovo_pvals <- tree_philr_node_pvals(orig_denovo_philr, denovo_tree_ps@phy_tree)
+cln_denovo_pvals <- apply(cln_denovo_philr, 2, function(x) {t.test(x, metad_col)$p.value})
 
 ref_ps_orig_philr <- philr::philr(df = ref_ps@otu_table + 1,
                             tree = ref_ps@phy_tree)
-ref_ps_orig_pvals <- tree_philr_node_pvals(ref_ps_orig_philr, ref_ps@phy_tree)
+ref_ps_orig_pvals <- apply(ref_ps_orig_philr, 2, function(x) {t.test(x, metad_col)$p.value})
 
-# my_groups <- c(rep("ref_cln", len), rep("upgma_cln", ))
+pdf(file = file.path(output_dir, "graphics", "pval_vs_pval_plots.pdf"))
+hist(ref_ps_clean_pvals, breaks = 50)
+hist(ref_ps_orig_pvals, breaks = 50)
+hist(cln_denovo_pvals, breaks = 50)
 
-log10_pval_pval_plot <- function(pvals_1, label_1, pvals_2, label_2) {
-# function for making pval plots
-  max_len <- min(length(pvals_1), length(pvals_2))
-  my_cor <- cor(sort(pvals_1[1:max_len]), sort(pvals_2[1:max_len]))
-  plot_data <- data.frame(pval1 = sort(log10(sample(pvals_1, max_len, replace = F))), 
-                          pval2 = sort(log10(sample(pvals_2, max_len, replace = F))))
-    g <- ggplot2::ggplot(data = plot_data, aes(x = pval1, y = pval2)) +  
-      ggplot2::ggtitle(paste(project, "Random trees vs non-random-trees")) +
-      ggplot2::geom_point() +
-      ggplot2::geom_abline(intercept = 0, slope = 1) +
-      ggplot2::theme_classic() +
-      ggplot2::ylab(label_1) +
-      ggplot2::xlab(label_2) +
-      ggplot2::labs(color = "Part weight")
-  return(g)
+my_stool <- which(metadata$type == "STOOL")
+my_swab <- which(metadata$type == "SWAB")
+
+my_table <- ref_ps_clean_philr
+
+my_stool_df <- ref_ps_clean_philr[my_stool,]
+my_swab_df <- ref_ps_clean_philr[my_swab,]
+
+hist(my_stool_df, breaks = 50)
+hist(my_swab_df, breaks = 50)
+
+num_col_used <- 10
+num_factors <- length(unique(metadata$type))
+
+my_values <- list()
+my_label <- c()
+
+index <- 1
+for (i in 1:ncol(my_table)){
+  my_stool_df <- my_table[my_stool,i]
+  my_swab_df <- my_table[my_swab,i]
+  my_values[index] <- data.frame(my_stool_df)
+  my_label[index] <- paste0("stool_n", i)
+  index <- index + 1
+  my_values[index] <- data.frame(my_swab_df)
+  my_label[index] <- paste0("swab_n", i)
+  index <- index + 1
 }
+names(my_values) <- my_label
+boxplot(my_values, col = c("red", "blue"), las=2,
+        main = paste(project, "ref_ps_clean_philr, Type"))
 
-plot1 <- log10_pval_pval_plot(ref_ps_clean_pvals, "ref_ps_clean_pvals",
-                              cln_denovo_pvals, "cln_denovo_pvals")
+#doing sex now
+my_stool <- which(metadata$sex == "male")
+my_swab <- which(metadata$sex == "female")
 
-plot2 <- log10_pval_pval_plot(ref_ps_clean_pvals, "ref_ps_clean_pvals",
-                              cln_denovo_pvals, "cln_denovo_pvals")
+my_table <- ref_ps_clean_philr
+my_values <- list()
+my_label <- c()
+
+index <- 1
+for (i in 1:ncol(my_table)){
+  my_stool_df <- my_table[my_stool,i]
+  my_swab_df <- my_table[my_swab,i]
+  my_values[index] <- data.frame(my_stool_df)
+  my_label[index] <- paste0("male_n", i)
+  index <- index + 1
+  my_values[index] <- data.frame(my_swab_df)
+  my_label[index] <- paste0("female_n", i)
+  index <- index + 1
+}
+names(my_values) <- my_label
+boxplot(my_values, col = c("red", "blue"), las=2,
+        main = paste(project, "ref_ps_clean_philr, Sex"))
 
 
-plot(sort(log10(cln_denovo_pvals_short)), sort(log10(ref_ps_clean_pvals)),
-     main = paste("philr ref vs upgma Rsqrd:", round(my_cor^2, 3)))
+# for (i in 1:ncol(my_table)){
+#   for (ii in unique(metadata[,"type"])){
+#     my_meta <- which(metadata$type == ii)
+#     my_values[index] <- data.frame(my_table[my_meta,i])
+#     my_label[index] <- paste0( ii, "_n", i)
+#     index <- index + 1
+#   }
+# }
 
-cln_denovo_pvals_short <- cln_denovo_pvals[1:length(ref_ps_clean_pvals)]
+for (clmn in 1:ncol(ref_ps_clean_philr)){
+}
+#verify qqplots by hand (histogram or node by node)
+#Are these pvals reasonable?
+#check against other metadata columns
+#run the other dataset
+stats::qqplot(ref_ps_clean_pvals, ref_ps_orig_pvals,
+              main = paste("qqplot", "ref_ps_clean_pvals", "ref_ps_orig_philr"))
+log10_pval_pval_plot(ref_ps_orig_pvals, "ref_ps_orig_pvals",
+                     ref_ps_clean_pvals, "ref_ps_clean_pvals", metad_col)
+stats::qqplot(ref_ps_clean_pvals, cln_denovo_pvals,
+              main = paste("qqplot", "ref_ps_clean_pvals", "cln_denovo_pvals"))
+log10_pval_pval_plot(ref_ps_clean_pvals, "ref_ps_clean_pvals",
+                     cln_denovo_pvals, "cln_denovo_pvals", metad_col)
+stats::qqplot(ref_ps_orig_pvals, cln_denovo_pvals,
+              main = paste("qqplot", "ref_ps_clean_pvals", "cln_denovo_pvals"))
+log10_pval_pval_plot(cln_denovo_pvals, "cln_denovo_pvals",
+                     ref_ps_clean_pvals, "ref_ps_clean_pvals", metad_col)
 
-my_cor <- cor(sort(cln_denovo_pvals_short), sort(ref_ps_clean_pvals))
+print("Generate random tree p values")
+ref_cln_rand_pvals <- lapply(cln_ref_rand_list, function(x) {
+  apply(x@otu_table, 2, function(y) {t.test(y, metad_col)$p.value})})
+cln_upgma_rand_pvals <- lapply(cln_upgma_rand_list, function(x) {
+  apply(x@otu_table, 2, function(y) {t.test(y, metad_col)$p.value})})
+ref_orig_rand_pvals <- lapply(orig_ref_rand_list, function(x) {
+  apply(x@otu_table, 2, function(y) {t.test(y, metad_col)$p.value})})
 
-# test <- data.frame(, ref_ps_clean_pvals)
-
-plot(sort(log10(cln_denovo_pvals_short)), sort(log10(ref_ps_clean_pvals)),
-     main = paste("philr ref vs upgma Rsqrd:", round(my_cor^2, 3)))
-
-ref_rand_pvals <- lapply(ref_rand_list, function(x) tree_philr_node_pvals(philr::philr(df = x@otu_table,
-                                                                                        tree = x@phy_tree), x@phy_tree))
-upgma_rand_pvals <- lapply(cln_upgma_rand_list, function(x) tree_philr_node_pvals(philr::philr(df = x@otu_table,
-                                                                                         tree = x@phy_tree), x@phy_tree))
-upgma_vs_ref_rand_cor <- lapply(ref_rand_pvals, function(x){
+print("Generate random tree p values")
+cln_upgma_vs_ref_cln_rand_cor <- lapply(ref_cln_rand_pvals, function(x){
   len <- min(length(x), length(cln_denovo_pvals))
-  cor(sort(cln_denovo_pvals[1:len]), sort(x[1:len]))
+  cor(sort(sample(cln_denovo_pvals, len)), sort(sample(x, len)))
 })
 
-upgma_vs_upgma_rand_cor <- lapply(upgma_rand_pvals, function(x){
+cln_upgma_vs_cln_upgma_rand_cor <- lapply(cln_upgma_rand_pvals, function(x){
   len <- min(length(x), length(cln_denovo_pvals))
-  cor(sort(cln_denovo_pvals[1:len]), sort(x[1:len]))
+  cor(sort(sample(cln_denovo_pvals, len)), sort(sample(x, len)))
 })
 
-ref_vs_ref_rand_cor <- lapply(ref_rand_pvals, function(x){
-  len <- min(length(x), length(ref_ps_clean_pvals))
-  cor(sort(ref_ps_clean_pvals[1:len]), sort(x[1:len]))
+cln_upgma_vs_orig_ref_rand_cor <- lapply(ref_orig_rand_pvals, function(x){
+  len <- min(length(x), length(cln_denovo_pvals))
+  cor(sort(sample(cln_denovo_pvals, len)), sort(sample(x, len)))
 })
 
-ref_vs_upgma_rand_cor <- lapply(upgma_rand_pvals, function(x){
+ref_cln_vs_ref_cln_rand_cor <- lapply(ref_cln_rand_pvals, function(x){
   len <- min(length(x), length(ref_ps_clean_pvals))
-  cor(sort(ref_ps_clean_pvals[1:len]), sort(x[1:len]))
+  cor(sort(sample(ref_ps_clean_pvals, len)), sort(sample(x, len)))
+})
+#
+ref_cln_vs_cln_upgma_rand_cor <- lapply(cln_upgma_rand_pvals, function(x){
+  len <- min(length(x), length(ref_ps_clean_pvals))
+  cor(sort(sample(ref_ps_clean_pvals, len)), sort(sample(x, len)))
+})
+
+ref_cln_vs_ref_orig_rand_cor <- lapply(ref_orig_rand_pvals, function(x){
+  len <- min(length(x), length(ref_ps_clean_pvals))
+  cor(sort(sample(ref_ps_clean_pvals, len)), sort(sample(x, len)))
+})
+
+ref_orig_vs_ref_cln_rand_cor <- lapply(ref_cln_rand_pvals, function(x){
+  len <- min(length(x), length(ref_ps_clean_pvals))
+  cor(sort(sample(ref_ps_orig_pvals, len)), sort(sample(x, len)))
+})
+
+ref_orig_vs_cln_upgma_rand_cor <- lapply(cln_upgma_rand_pvals, function(x){
+  len <- min(length(x), length(ref_ps_clean_pvals))
+  cor(sort(sample(ref_ps_orig_pvals, len)), sort(sample(x, len)))
+})
+
+ref_orig_vs_ref_orig_rand_cor <- lapply(ref_orig_rand_pvals, function(x){
+  len <- min(length(x), length(ref_ps_clean_pvals))
+  cor(sort(sample(ref_ps_orig_pvals, len)), sort(sample(x, len)))
 })
 
 print("Munging output for boxpots")
-bp_labels <- c(rep("upgma_vs_ref_rand_cor",length(upgma_vs_ref_rand_cor)),
-               rep("upgma_vs_upgma_rand_cor",length(upgma_vs_upgma_rand_cor)),
-               rep("ref_vs_ref_rand_cor",length(ref_vs_ref_rand_cor)),
-               rep("ref_vs_upgma_rand_cor",length(ref_vs_upgma_rand_cor)))
-cor_vals <- c(unlist(upgma_vs_ref_rand_cor),
-              unlist(upgma_vs_upgma_rand_cor),
-              unlist(ref_vs_ref_rand_cor),
-              unlist(ref_vs_upgma_rand_cor))
+bp_labels <- c(rep("cln_upgma_vs_ref_cln_rand_cor",length(cln_upgma_vs_ref_cln_rand_cor)),
+               rep("cln_upgma_vs_cln_upgma_rand_cor",length(cln_upgma_vs_cln_upgma_rand_cor)),
+               rep("cln_upgma_vs_orig_ref_rand_cor",length(cln_upgma_vs_orig_ref_rand_cor)),
+               rep("ref_cln_vs_ref_cln_rand_cor",length(ref_cln_vs_ref_cln_rand_cor)),
+               rep("ref_cln_vs_cln_upgma_rand_cor",length(ref_cln_vs_cln_upgma_rand_cor)),
+               rep("ref_cln_vs_ref_orig_rand_cor",length(ref_cln_vs_ref_orig_rand_cor)),
+               rep("ref_orig_vs_ref_cln_rand_cor",length(ref_orig_vs_ref_cln_rand_cor)),
+               rep("ref_orig_vs_cln_upgma_rand_cor",length(ref_orig_vs_cln_upgma_rand_cor)),
+               rep("ref_orig_vs_ref_orig_rand_cor",length(ref_orig_vs_ref_orig_rand_cor)))
+
+cor_vals <- c(unlist(cln_upgma_vs_ref_cln_rand_cor),
+              unlist(cln_upgma_vs_cln_upgma_rand_cor),
+              unlist(cln_upgma_vs_orig_ref_rand_cor),
+              unlist(ref_cln_vs_ref_cln_rand_cor),
+              unlist(ref_cln_vs_cln_upgma_rand_cor),
+              unlist(ref_cln_vs_ref_orig_rand_cor),
+              unlist(ref_orig_vs_ref_cln_rand_cor),
+              unlist(ref_orig_vs_cln_upgma_rand_cor),
+              unlist(ref_orig_vs_ref_orig_rand_cor))
+
 plot_data <- data.frame(bp_labels, cor_vals)
 
 g <- ggplot2::ggplot(data = plot_data, aes(y = bp_labels, x = cor_vals)) +  
   ggplot2::geom_boxplot() +
-  ggplot2::ggtitle(paste(project, "Random trees vs non-random-trees")) +
+  ggplot2::ggtitle(paste(project, "Random trees vs non-random-trees, metad:", metad_col)) +
   # ggplot2::geom_hline(yintercept = 0) +
-  # ggplot2::theme(axis.text.x = element_text(angle = 45),
-  #                axis.text.y = element_text(angle = 45)) +
+  ggplot2::theme(axis.text.x = element_text(angle = 45),
+                 axis.text.y = element_text(angle = 45)) +
   ggplot2::theme_classic() +
   # ggplot2::scale_y_discrete(labels = seq(0, 1, by = 0.2)) +
   # ggplot2::ylab("AUC") +
@@ -261,4 +385,4 @@ g <- ggplot2::ggplot(data = plot_data, aes(y = bp_labels, x = cor_vals)) +
   ggplot2::labs(color = "Part weight")
 print(g)
 
-
+dev.off()
