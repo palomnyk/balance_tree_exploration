4e8c71d7e3dbdf222ba500cac4dfe70c3ce1ce36
Aaron_Y <amyerke@uncc.edu>
2021-11-19 13:13:19 -0500

    metadata and scripts for downloading NJ seq data

---
+++ Noguera-Julian/scripts/download_scripts/download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "hi"
+mkdir logs; download.sh &> logs/download.log
+
+echo "in $(pwd) directory"
+
+module load sra-tools  
+
+META=../../SraAccList.txt
+COL=1
+
+NUM=$( < $META wc -l)
+echo "Table $META has $NUM rows including the hearder."
+echo "Taking SRA accession id's from column: $COL."
+
+# echo "First row:"
+# head -n 2 $META
+
+for i in $(seq 1 $NUM)
+do
+    echo "$i of $NUM"
+    SRR=$(sed -ne "${i}p" ${META} | cut -f ${COL})
+    SRR="$(echo -e "${SRR}" | tr -d '[:space:]')"
+    echo "Getting ${SRR}..."
+    #export SRR
+    #qsub -v SRR -q copperhead doDownload.pbs
+    nohup fasterq-dump $SRR -O ../../downloaded_seqs
+    # echo " ... gzip ... "
+    # gzip ${SRR}*
+    echo "Done with $SRR."
+done
---
+++ Noguera-Julian/scripts/download_scripts/sra_download.R
@@ -0,0 +1,54 @@
+# Author: Aaron Yerke
+# Script to download from the SRA
+# This was helepful: https://github.com/jsilve24/philr/blob/master/vignettes/philr-intro.Rmd#L142
+
+rm(list = ls()) #clear workspace
+
+##-Load Depencencies------------------------------------------------##
+
+##-Establish directory layout---------------------------------------##
+home_dir <- file.path('~','git','balance_tree_exploration')
+project <- "Noguera-julian"
+download_dir <- file.path(home_dir, project, "downloaded_seqs")
+sra_run_table <- read.table(file.path(home_dir, project, "SraRunTable.txt"),
+                            sep = ",",
+                            header = TRUE)
+
+my_rows <- sra_run_table$Assay.Type == "AMPLICON"
+my_accessions <- sra_run_table$Run[my_rows]
+
+print(paste("number of rows:", length(my_accessions)))
+
+downloaded_files <- list.files(path = download_dir)
+print(paste("number of files already downloaded:", length(downloaded_files)))
+
+print(paste("need to download this many more:", length(my_accessions) - length(downloaded_files)))
+
+##-Download SRA files ----------------------------------------------##
+setwd(download_dir)
+for (run in my_accessions) {
+  
+  my_file <- paste0(run, ".fastq.gz")
+  if (!my_file %in% downloaded_files){
+    print(paste("attempting download of:", run))
+    my_command <- paste("module load sra-tools ;",
+                        "nohup fasterq-dump", run, 
+                        "-O",  download_dir)
+    print("my command:", my_command)
+    system2(command = my_command, wait = TRUE)
+    
+  }else{
+    print(paste(run, "was already there!"))
+  }
+  Sys.sleep(1)  
+}  
+  
+# bash download commands:
+# module load sra-tools
+# nohup fasterq-dump $SRR -O ../../downloaded_seqs
+# 
+# Output format:
+#   SRR5799852_1.fastq.gz
+
+
+
