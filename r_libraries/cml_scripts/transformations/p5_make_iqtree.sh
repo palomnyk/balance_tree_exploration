2c714e91728798d58dc5a63e25c1380947e0f6b5
Aaron_Y <amyerke@uncc.edu>
2022-01-16 22:23:59 -0500

    bash script for making IQ_tree tree

---
+++ r_libraries/cml_scripts/transformations/p5_make_iqtree.sh
@@ -0,0 +1,27 @@
+#Script from creating IQTree on cluster - requires CLUSTALW and IQTREE modules
+
+#Getting fancy with color in print statements
+RED='\033[0;31m' #for print red
+NC='\033[0m' # No Color
+
+home_dir=$1 #first comandline argument is the project name
+project=$2
+
+echo "Found arguments ${home_dir} and ${project}."
+
+#my_fasta is made from 
+my_fasta=$home_dir/$project/output/tree_process_blast/dada2seqs.fasta
+
+#check for fastq file that is our starting point
+if [ -f "$my_fasta" ]; then
+    echo "$my_fasta exists."
+else 
+    printf "${RED}$my_fasta does not exist,\nrun your sequences through Dada2 and create the fastq with p1 and p2${NC}"
+fi
+
+#align the sequences with clustal omega
+module load clustal-omega
+clustal-omega -in $my_fasta -out aligned_dada2seqs.fasta -fasta
+
+
+
