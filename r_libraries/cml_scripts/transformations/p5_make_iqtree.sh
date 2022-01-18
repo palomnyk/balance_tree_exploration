99f3fc9b66a48a7e68b5cd9ea5209ac7d778dccc
Aaron_Y <amyerke@uncc.edu>
2022-01-17 20:02:33 -0500

    Change protocol to use the DECIPHER output from p1.

--- r_libraries/cml_scripts/transformations/p5_make_iqtree.sh
+++ r_libraries/cml_scripts/transformations/p5_make_iqtree.sh
@@ -11,7 +11,7 @@ echo "Found arguments ${home_dir} and ${project}."
 
 #my_fasta is made from 
 my_fasta=$home_dir/$project/output/tree_process_blast/dada2seqs.fasta
-clust_out=$home_dir/$project/output/trees/aligned_dada2seqs.fasta
+clust_out=$home_dir/$project/output/trees/ForwardReads_DADA2_taxonomy.aln
 
 #check for fastq file that is our starting point
 if [ -f "$my_fasta" ]; then
@@ -20,21 +20,21 @@ else
     printf "${RED}$my_fasta does not exist,\nrun your sequences through Dada2 and create the fastq with p1 and p2${NC}"
 fi
 
-#check if $home_dir/$project/output/trees/ exists and if not, create it and go to it
-if [ -f "$home_dir/$project/output/trees/" ]; then
-    echo " $home_dir/$project/output/trees/ exists."
-else 
-    printf "${RED} $home_dir/$project/output/trees/ does not exist, creating it now${NC}"
-    mkdir  $home_dir/$project/output/trees/
-fi
+# #check if $home_dir/$project/output/trees/ exists and if not, create it and go to it
+# if [ -f "$home_dir/$project/output/trees/" ]; then
+#     echo " $home_dir/$project/output/trees/ exists."
+# else 
+#     printf "${RED} $home_dir/$project/output/trees/ does not exist, creating it now${NC}"
+#     mkdir  $home_dir/$project/output/trees/
+# fi
 cd $home_dir/$project/output/trees/
-
-#align the sequences with clustal omega
-module load clustal-omega
-clustalo --in=$my_fasta --out=$clust_out
-
-printf "${RED}Clustalo has run.${NC} Attempting to run iqtree"
-module unload clustal-omega
+# 
+# #align the sequences with clustal omega
+# module load clustal-omega
+# clustalo --in=$my_fasta --out=$clust_out
+# 
+# printf "${RED}Clustalo has run.${NC} Attempting to run iqtree"
+# module unload clustal-omega
 module load iqtree
 
 iqtree2 -s $clust_out -T AUTO
