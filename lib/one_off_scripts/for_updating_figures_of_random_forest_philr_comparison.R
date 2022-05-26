35bcf8ce681062f57060de8b88f83a4f83ab3356
Aaron_Y <amyerke@uncc.edu>
2022-05-25 22:04:52 -0400

    checkpoint before diving in to fix bug

--- lib/one_off_scripts/for_updating_figures_of_random_forest_philr_comparison.R
+++ lib/one_off_scripts/for_updating_figures_of_random_forest_philr_comparison.R
@@ -38,23 +38,22 @@ library("ggplot2")
 
 ##-Establish directory layout---------------------------------------##
 home_dir <- file.path('~','git','balance_tree_exploration')
-project <- "Zeller"
+project <- "Vanderbilt"
 output_dir <- file.path(home_dir, project, 'output')
-setwd(file.path(home_dir))
 
 ##-Set up constants-------------------------------------------------##
 num_cycles <- 20
 if(num_cycles < 3) stop("num_cycles should be 3 or more")
-main_output_label <- paste0("auc_rand_v_ref_v_upgma_v_raw_vert_", num_cycles)
-philr_taxa_weights <- c("uniform","gm.counts","anorm","anorm.x.gm.counts","enorm","enorm.x.gm.counts")
-philr_ilr_weights <- c("uniform","blw","blw.sqrt","mean.descendants")
+main_output_text <- "random_forest_auc_R"
+main_output_label <- "random_forest_auc_R_20.csv"
+# philr_taxa_weights <- c("uniform","gm.counts","anorm","anorm.x.gm.counts","enorm","enorm.x.gm.counts")
+# philr_ilr_weights <- c("uniform","blw","blw.sqrt","mean.descendants")
+philr_taxa_weights <- c("enorm")
+philr_ilr_weights <- c("blw")
 random_seed <- 36
 
-all_plot_data <- data.frame(read.table(file = file.path(output_dir, "tables", 
-                                             paste0(main_output_label, ".csv")),
-            sep = ",", header = TRUE))
-
-unique(all_plot_data$rf_type)
+all_plot_data <- read.table(file = file.path(output_dir, "tables", main_output_label),
+            sep = ",", header = TRUE)
 
 best_seqs <- table(all_plot_data$rf_imp_seq)
 
@@ -167,12 +166,12 @@ for (mta in 1:length(unique(all_plot_data$metadata_col))){
   message(my_meta)
   #select plot data for each metadata cat
   plot_data <- all_plot_data[all_plot_data$metadata_col == my_meta,]
-  
-  # need to organize the boxes so that count tables are first - these 
-  # will not have weighting schemes, therefor select those which do not have 
+
+  # Organize the boxes so that count tables are first - these will not 
+  # have weighting schemes, therefor select those which do not have 
   # "uniform"
-  not_uniform_tw <- which(plot_data$taxa_weight != "uniform" )
-  not_uniform_iw <- which(plot_data$ilr_weight != "uniform" )
+  not_uniform_tw <- which(plot_data$taxa_weight != philr_taxa_weights[1] )
+  not_uniform_iw <- which(plot_data$ilr_weight != philr_ilr_weights[1] )
   philr_ds <- unique(plot_data$trans_group[c(not_uniform_tw,not_uniform_iw)] ) #pulls out philr only data
   non_philr_ds <- unique(plot_data$trans_group[ !(plot_data$trans_group %in% philr_ds)])
   non_philr_ds_pd <- subset(plot_data, trans_group %in% non_philr_ds)
@@ -182,14 +181,28 @@ for (mta in 1:length(unique(all_plot_data$metadata_col))){
   new_pd <- rbind(non_philr_ds_pd, philr_ds_pd)#reorganize
   new_pd$trans_group <- factor(new_pd$trans_group, levels = c(non_philr_ds, philr_ds))#put non-philr first
   meta_mean <- mean(new_pd$all_auc)
+  #add number to the end of each boxplot label
+  my_labels <- as.character(levels(new_pd$trans_group))
+  # my_new_labels <- lapply()
+  # print(my_labels)
+  counter <- 0
+  random_tree <- FALSE
+  new_labels <- sapply(my_labels, function(x) {
+    counter <<- counter + 1
+    return(paste0(x, "_", counter))
+  })
+  
+  # print(length(new_labels))
+  
   print(meta_mean)
 	g <- ggplot2::ggplot(new_pd, aes(y = all_auc, x = trans_group, group=trans_group)) + 
 		ggplot2::geom_boxplot() +
 		ggplot2::ggtitle(label = paste(project, my_meta)) +
 		ggplot2::geom_hline(yintercept = meta_mean, color="red") +
+	  # scale_fill_discrete(labels=new_labels) +
 		# ggplot2::ggtitle( label = paste("num_tg:", length(unique(new_pd$trans_group)))) +
 		ggplot2::theme_classic() +
-		ggplot2::scale_x_discrete(guide = guide_axis(angle = 90)) +
+		ggplot2::scale_x_discrete(guide = guide_axis(angle = 90), labels=new_labels) +
 		ggplot2::ylab("AUC") +
 		ggplot2::xlab("Tree type")
 	print(g)
