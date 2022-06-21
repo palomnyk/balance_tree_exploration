de4a0979319089217ead5c1798edc624da64f8b8
Aaron_Y <amyerke@uncc.edu>
2022-06-21 18:15:03 -0400

    add flush to print statements, increase time

--- lib/cml_scripts/random_forest/random_forest_manual_train.py
+++ lib/cml_scripts/random_forest/random_forest_manual_train.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-
 # Author: Aaron Yerke, aaronyerke@gmail.com
 # This is a script for assessing the various compositional data 
 # transformations against the random forest
 # --------------------------------------------------------------------------
-print("Loading external libraries.")
+print("Loading external libraries.",flush = True)
 # --------------------------------------------------------------------------
 import os, sys
 from statistics import mean
@@ -29,7 +28,7 @@ import argparse
 import random
 
 # --------------------------------------------------------------------------
-print("Reading commmandline input with optparse.")
+print("Reading commmandline input with optparse.", flush = True)
 # --------------------------------------------------------------------------
 
 parser = argparse.ArgumentParser(description="This script runs a random forest test on various datasets.")
@@ -61,7 +60,7 @@ parser.add_argument("-t", "--training", default=0.9,
 options, unknown = parser.parse_known_args()
 
 # --------------------------------------------------------------------------
-print("Establishing directory layout.")
+print("Establishing directory layout.", flush = True)
 # --------------------------------------------------------------------------
 home_dir = os.path.expanduser(options.homedir)
 project = options.project
@@ -73,10 +72,10 @@ def df_factory(my_path, my_sep):
 		df =  pd.read_csv(my_path, sep=my_sep, header=0, index_col=0)
 		return df
 	except:
-		print(f"An exception occurred during creation of dataframe from {my_path}")
+		print(f"An exception occurred during creation of dataframe from {my_path}", flush = True)
 
 # --------------------------------------------------------------------------
-print("Establishing other constants")
+print("Establishing other constants", flush = True)
 # --------------------------------------------------------------------------
 seed = 7
 scoring = "AUC"
@@ -87,17 +86,17 @@ col_names = ["dataset", "metadata"]
 num_iterations = 10
 col_names = col_names + [f"split{x}" for x in range(num_iterations)]
 print(col_names)
-pdf_fpath = os.path.join(output_dir, "graphics", f"bp_{main_output_label}_{project}.pdf")
+pdf_fpath = os.path.join(output_dir, "graphics", f"bp_{main_output_label}_{project}.pdf", flush = True)
 
 # --------------------------------------------------------------------------
-print("Importing data to working env.")
+print("Importing data to working env.", flush = True)
 # --------------------------------------------------------------------------
 meta_df = pd.read_csv(os.path.expanduser(os.path.join(home_dir, project, str(options.meta_fn))), \
 	sep=options.delim, header=0, index_col=options.meta_index_col)
 metad_cols = range(len(meta_df.columns))
 
 # ----------------------------------------------------------------------------
-print("Setting up tables to feed the random forest model.")
+print("Setting up tables to feed the random forest model.", flush = True)
 # --------------------------------------------------------------------------
 tables = []
 tables.append(("DaDa2", (os.path.join(output_dir, "tables", "ForwardReads_DADA2.txt"),"\t")))
@@ -110,12 +109,12 @@ tables.append(("clr_DADA2", (os.path.join(output_dir, "tables", "clr_asv.csv"), 
 tables.append(("clr_HashSeq", (os.path.join(output_dir,"tables", "clr_hashseq.csv"), ",")))
 tables.append(("Silva_ref_counts_only", (os.path.join(output_dir,"tables", "Silva_ref_counts.csv"), ",")))
 
-print(len(tables))
+print(len(tables), flush = True)
 philr_part_weights = ["anorm","enorm"]
 philr_ilr_weights = ["blw.sqrt","mean.descendants"]
 silva_philr_dir = os.path.join(output_dir, "tables", "silva_philr_weights")
 if not os.path.exists(silva_philr_dir):
-  print(f"{silva_philr_dir} does not exist. Use silva_philr_weights.R to create it.")
+  print(f"{silva_philr_dir} does not exist. Use silva_philr_weights.R to create it.", flush = True)
   sys.exit()
 for pw in philr_part_weights:
 	for iw in philr_ilr_weights:
@@ -130,7 +129,7 @@ for pw in philr_part_weights:
 # # meta_df.head
 
 # --------------------------------------------------------------------------
-print(f"Running random forest model to find {scoring}.")
+print(f"Running random forest model to find {scoring}.", flush = True)
 # --------------------------------------------------------------------------
 with open(result_fpath, "w+") as fl:
 	fl.write(",".join(col_names))
@@ -157,11 +156,11 @@ with open(result_fpath, "w+") as fl:
 			msg = f"{name},{m_c},{final_acc}\n"
 			print(msg)
 			fl.write(msg)
-		print(f"{name}, {m_c} mean is: {mean(my_accuracy)}")
-print("Finished recording accuracy.")
+		print(f"{name}, {m_c} mean is: {mean(my_accuracy)}", flush = True)
+print("Finished recording accuracy.", flush = True)
 
 # --------------------------------------------------------------------------
-print(f"Building boxplot PDF.")
+print(f"Building boxplot PDF.", flush = True)
 # --------------------------------------------------------------------------
 #Setup for building boxplots
 result_df = pd.read_csv(result_fpath, sep=',', header=0)
@@ -172,7 +171,7 @@ num_rows = abs(-len(tables)//num_cols)
 pdf = matplotlib.backends.backend_pdf.PdfPages(pdf_fpath)
 for meta_c in metadata_cats:
 	fig = plt.figure(figsize=(11,11))
-	fig.suptitle(f"{project} random forest manual {train_percent}training {scoring} {meta_c}")
+	fig.suptitle(f"{project} random forest manual {train_percent}training {scoring} {meta_c}", flush = True)
 	plt.subplots_adjust(bottom=0.8)
 	meta_result_df = pd.DataFrame(result_df[result_df["metadata"] == meta_c])
 	# flat_num_only = pd.DataFrame(meta_result_df.iloc[:,5:]).to_numpy().flatten()
@@ -192,5 +191,5 @@ for meta_c in metadata_cats:
 	fig.tight_layout()
 	pdf.savefig( fig )
 
-print("Saving pdf")
+print("Saving pdf", flush = True)
 pdf.close()
