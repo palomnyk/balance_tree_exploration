b64d4ba86360228f2ec8020e0c34bd82dd67d7e3
Aaron_Y <amyerke@uncc.edu>
2022-07-02 19:51:43 -0400

    streamlined script

--- lib/cml_scripts/transformations/make_compositional_datasets.R
+++ lib/cml_scripts/transformations/make_compositional_datasets.R
@@ -53,10 +53,7 @@ option_list <- list(
   optparse::make_option(c("-l", "--metadata_delim"), type="character", default="\t",
                         help="metadata file deliminator", metavar="character"),
   optparse::make_option(c("-r", "--metadata_rowname"), type="character", default=NULL,
-                        help="metadata file row to use for row names", metavar="character"),
-  optparse::make_option(c("-n", "--num_cycles"), type="numeric", default=20,
-                        help="Number of times to shuffle data and run loop again", 
-                        metavar="character")
+                        help="metadata file row to use for row names", metavar="character")
 ); 
 
 opt_parser <- optparse::OptionParser(option_list=option_list);
@@ -75,50 +72,44 @@ source(file.path(home_dir, "lib", "statistical_functions.R"))
 source(file.path(home_dir, "lib", "table_manipulations.R"))
 
 ##-Set up constants-------------------------------------------------##
-num_cycles <- opt$num_cycles
-if(num_cycles < 3) stop("num_cycles should be 3 or more")
-main_output_text <- "auc_rand_v_ref_v_upgma_v_raw_vert_"
-main_output_label <- paste0("auc_rand_v_ref_v_upgma_v_raw_vert_", num_cycles)
-philr_taxa_weights <- c("uniform","gm.counts","anorm","anorm.x.gm.counts","enorm","enorm.x.gm.counts")
-philr_ilr_weights <- c("uniform","blw","blw.sqrt","mean.descendants")
 random_seed <- 36
 
-print("Importing and preprocessing tables.")
-asv_table <- data.frame(readRDS(file.path(output_dir, "r_objects", "ForwardReads_DADA2.rds")))
-# hashseq <- data.frame(data.table::fread(file = file.path(output_dir,"hashseq", "hashseq.csv"),
-#                                         header="auto", data.table=FALSE), row.names = 1)
+# --------------------------------------------------------------------------
+print("Importing and preprocessing tables, starting with DADA2")
+# --------------------------------------------------------------------------
+initial_table <- data.frame(readRDS(file.path(output_dir, "r_objects", "ForwardReads_DADA2.rds")))
 
 print("creating DADA2 lognorm, ALR and CLR")
-if (dir.exists(file.path(output_dir,"r_objects", "lognorm_asv.rds"))) {
-  ln_asv_tab <- readRDS(file.path(output_dir,"r_objects", "lognorm_asv.rds"))
-}else{
-  ln_asv_tab <- lognorm(asv_table)
-  saveRDS(ln_asv_tab, file = file.path(output_dir,"r_objects", "lognorm_asv.rds"))
-  write.csv(ln_asv_tab, file = file.path(output_dir,"tables", "lognorm_dada2.csv"))
+if (!dir.exists(file.path(output_dir,"r_objects", "lognorm_asv.rds"))) {
+  df <- lognorm(initial_table)
+  saveRDS(df, file = file.path(output_dir,"r_objects", "lognorm_asv.rds"))
+  write.csv(df, file = file.path(output_dir,"tables", "lognorm_dada2.csv"))
 }
-my_zeros <- apply(asv_table, 2, function(x) {
+my_zeros <- apply(initial_table, 2, function(x) {
   return(sum(x == 0))
 })
 alr_col <- which(my_zeros == min(my_zeros))[1]
-# alr_col_num <- grep(alr_col, colnames(asv_table))
+# alr_col_num <- grep(alr_col, colnames(initial_table))
 print("creating DADA2 ALR")
-if (file.exists(file.path(output_dir,"r_objects", "alr_asv.rds"))) {
-  DADA2_alr <- readRDS(file.path(output_dir,"r_objects", "alr_asv.rds"))
-}else{
-  DADA2_alr <- as.data.frame(rgr::alr(as.matrix(asv_table + 1), j = as.numeric(alr_col)))
-  saveRDS(DADA2_alr, file = file.path(output_dir,"r_objects", "alr_asv.rds"))
-  write.csv(DADA2_alr, file = file.path(output_dir,"tables", "alr_asv.csv"))
+if (!file.exists(file.path(output_dir,"r_objects", "alr_asv.rds"))) {
+  df <- as.data.frame(rgr::alr(as.matrix(initial_table + 1), j = as.numeric(alr_col)))
+  saveRDS(df, file = file.path(output_dir,"r_objects", "alr_asv.rds"))
+  write.csv(df, file = file.path(output_dir,"tables", "alr_asv.csv"))
 }
 print("creating DADA2 CLR")
-if (dir.exists(file.path(output_dir,"r_objects", "clr_asv.rds"))) {
-  DADA2_clr <- readRDS(file.path(output_dir,"r_objects", "clr_asv.rds"))
-}else{
-  DADA2_clr <- as.data.frame(rgr::clr(as.matrix(asv_table + 1)))
-  saveRDS(DADA2_clr, file = file.path(output_dir,"r_objects", "clr_asv.rds"))
-  write.csv(DADA2_clr, file = file.path(output_dir,"tables", "clr_asv.csv"))
+if (!dir.exists(file.path(output_dir,"r_objects", "clr_asv.rds"))) {
+  df <- as.data.frame(rgr::clr(as.matrix(initial_table + 1)))
+  saveRDS(df, file = file.path(output_dir,"r_objects", "clr_asv.rds"))
+  write.csv(df, file = file.path(output_dir,"tables", "clr_asv.csv"))
 }
 
+# --------------------------------------------------------------------------
+print("Now creating ")
+# --------------------------------------------------------------------------
+
 # print("Creating hashseq lognorm, ALR and CLR.")
+# hashseq <- data.frame(data.table::fread(file = file.path(output_dir,"hashseq", "hashseq.csv"),
+#                                         header="auto", data.table=FALSE), row.names = 1)
 # if (dir.exists(file.path(output_dir,"r_objects", "lognorm_hashseq.rds"))) {
 #   ln_hs_tab <- readRDS(file.path(output_dir,"r_objects", "lognorm_HashSeq.rds"))
 # }else{
@@ -146,8 +137,5 @@ if (dir.exists(file.path(output_dir,"r_objects", "clr_asv.rds"))) {
 #   saveRDS(HashSeq_alr, file = file.path(output_dir,"r_objects", "alr_hashseq.rds"))
 #   write.csv(HashSeq_alr, file = file.path(output_dir,"tables", "alr_hashseq.csv"))
 # }
-print("Making Silva control")
-ref_ps <- readRDS(file.path(output_dir, "r_objects", "ref_tree_phyloseq_obj.rds"))
-write.csv(ref_ps@otu_table, file.path(output_dir,"tables", "Silva_ref_counts.csv"))
 
 print("Reached end of script.")
