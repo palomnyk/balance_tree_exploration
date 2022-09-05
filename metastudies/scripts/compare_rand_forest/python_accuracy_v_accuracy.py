b7b257d72544ee88b0b81493cb310144590ca1bf
Aaron_Y <amyerke@uncc.edu>
2022-09-04 21:44:23 -0400

    add colors and shapes to plots

--- metastudies/scripts/compare_rand_forest/python_accuracy_v_accuracy.py
+++ metastudies/scripts/compare_rand_forest/python_accuracy_v_accuracy.py
@@ -34,7 +34,6 @@ print("Establishing directory layout.", flush = True)
 # --------------------------------------------------------------------------
 home_dir = os.path.expanduser(options.homedir)
 projects = ["Vanderbilt", "Vangay", "Zeller", "Noguera-Julian"]
-# projects = ["Vanderbilt", "Vangay"]
 output_dir = os.path.join(home_dir, "metastudies", "output")
 assert os.path.exists(output_dir)
 plot_pdf_fpath = os.path.join(output_dir, "accuracy_vs_accuracy_python_by_transformation.pdf")
@@ -44,10 +43,10 @@ print("Establishing other constants.", flush = True)
 font1 = {'family':'serif','color':'blue','size':20}
 font2 = {'family':'serif','color':'darkred','size':15}
 comp_ds = ['alr_DADA2', 'clr_DADA2', 'DaDa2', 'Filtered_IQtree', \
-	'Filtered_IQtree_blw.sqrt_anorm', 'Filtered_Silva_DADA2', \
-	'Filtered_Silva_DADA2_blw.sqrt_anorm', 'Filtered_UPGMA_DADA2', \
-	'Filtered_UPGMA_DADA2_blw.sqrt_anorm', 'lognorm_DADA2', 'Silva_DADA2', \
-	'Silva_DADA2_blw.sqrt_anorm']
+	'Filtered_IQtree_blw.sqrt_enorm', 'Filtered_Silva_DADA2', \
+	'Filtered_Silva_DADA2_blw.sqrt_enorm', 'Filtered_UPGMA_DADA2', \
+	'Filtered_UPGMA_DADA2_blw.sqrt_enorm', 'lognorm_DADA2', 'Silva_DADA2', \
+	'Silva_DADA2_blw.sqrt_enorm']
 
 pdf = matplotlib.backends.backend_pdf.PdfPages(plot_pdf_fpath)
 #set font sizes
@@ -62,7 +61,6 @@ for ds1 in comp_ds:
 		if (ds1 != ds2):
 			print(ds2)
 			train_percent = 0.75
-			pvals = {}
 			ds1_score = {}
 			ds2_score = {}
 			features = []
@@ -70,7 +68,7 @@ for ds1 in comp_ds:
 			print(f"{ds1} {ds2}")
 			for project in projects:
 				print(f"Adding project {project}")
-				main_output_label = f"sklearn_random_forest_manual_{train_percent}train_{project}_data"
+				main_output_label = f"sklearn_random_forest_manual_{train_percent}train"
 				op_dir = os.path.join(home_dir, project, "output")
 				result_fpath = os.path.join(op_dir, "tables", f"{main_output_label}.csv")
 				print(result_fpath)
@@ -81,33 +79,37 @@ for ds1 in comp_ds:
 				splits = ds1_table.columns[ds1_table.columns.str.startswith('split')].tolist()
 				# print(ds1_table[splits])
 				means = ds1_table[splits].agg(mean, axis = 1)
-				print(means)
 				assert not means.empty, f"{ds1} is not in the table from {project}"
 				for feat, ave in zip(list(ds1_table["metadata"].values) ,list(means)):
-					ds1_score[feat] = ave
+					ds1_score[f"{project}_{feat}"] = [ave, project]
 				#table 2
 				ds2_table = my_table.loc[my_table["dataset"] == ds2,]
 				splits = ds2_table.columns[ds2_table.columns.str.startswith('split')].tolist()
 				means = ds2_table[splits].agg(mean, axis = 1)
 				assert not means.empty, f"{ds2} is not in the table from {project}"
-				pval_fpath = os.path.join(op_dir, "tables", f"{project}_pValuesUnivariate_taxaVmetadata.csv")
-				my_table = pd.read_csv(pval_fpath, sep=',', header=0)
 				# print(my_table)
 				for feat, ave in zip(list(ds2_table["metadata"].values) ,list(means)):
-					ds2_score[feat] = ave
-					pvals[feat] = min(my_table.loc[(my_table["meta_name"]==feat) & (my_table["taxa_lev"] == "Genus"), "pval"].values) + 1e-80
+					ds2_score[f"{project}_{feat}"] = [ave, project]
 			print("build graphic")
-			print(pvals.values())
-			pval_log = np.log10(list(pvals.values()))
-			ds1_lst = np.array(list(ds1_score.values()))
-			ds2_lst = np.array(list(ds2_score.values()))
+			# same_keys = set(ds2_score.keys()).intersection(set(ds1_score.keys()))
+			# ds1_score = {key:ds1_score[key] for key in same_keys}
+			# ds2_score = {key:ds2_score[key] for key in same_keys}
+			ds1_lst = np.array(list(map(lambda x: x[0], list(ds1_score.values()))))
+			ds2_lst = np.array(list(map(lambda x: x[0], list(ds2_score.values()))))
 			a, b = np.polyfit(ds1_lst, ds2_lst, 1)
-			print(a, b)
+			# print(a, b)
 			fig = plt.figure(figsize=(11,11))
 			fig.suptitle(f"Metastudy {train_percent}training {ds1} vs {ds2} by accuracy, Python only")
 			plt.subplots_adjust(bottom=0.8)
 			ax = fig.add_subplot(1,1,1)
-			ax.scatter(ds1_score.values(), ds2_score.values(), color = "blue", label=f"{ds1} vs {ds2}")
+			my_projects = list(set(list(map(lambda x: x[1], list(ds2_score.values())))))#pulling second element from each dict.value
+			my_markers = ["o", "s", "P", "v", "x"]
+			for i, label in enumerate(list(ds2_score.keys())):
+				my_proj = ds2_score[label][1]
+				print(f"{my_proj} {ds1_lst[i]} {ds2_lst[i]}, {list(ds2_score.keys())[i]}")
+				my_marker = my_markers[my_projects.index(my_proj)]
+				ax.scatter(ds1_lst[i], ds2_lst[i], s=70, label=list(ds2_score.keys())[i], marker=my_marker)
+			# plt.annotate(label, (x_lst[i], y_lst[i]))
 			ax.plot([0,1], [0,1], color = "r", label = "expected")
 			ax.plot(ds1_lst, a*ds1_lst+b, color = "green", label = "accuracy polyfit")
 			ax.set_xlabel(f"Accuracy {ds1}")
@@ -116,9 +118,6 @@ for ds1 in comp_ds:
 			fig.tight_layout()
 			print("Saving figure to pdf", flush = True)
 			pdf.savefig( fig )
-			# sys.exit()
-			# print(my_table.columns)
-			# print(sorted(set(my_table["dataset"].values),key=lambda s: s.lower()))
 
 print("Saving pdf", flush = True)
 pdf.close()
