063d85f255bdc19d8de62c8dba8ed034367e7946
Aaron_Y <amyerke@uncc.edu>
2022-10-05 11:54:37 -0400

    add first boxplot and update summary boxplot

--- metastudies/scripts/mlm_metrics/all_sum_sklearn_ml_metrics_acc.py
+++ metastudies/scripts/mlm_metrics/all_sum_sklearn_ml_metrics_acc.py
@@ -85,8 +85,11 @@ scoring = "accuracy"
 col_names = ["metadata", "ilr_weight", "part_weight", "model", "split1", "split2", "split3", "split4", "split5", "split6", "split7", "split8", "split9", "split10"]
 model_symbols = ["|", "<", "*", "x", "o", "^", ","]
 
+bp_pdf_fpath = os.path.join(final_output_dir, f"bp_{output_label}.pdf")
 sum_pdf_fpath = os.path.join(final_output_dir, f"sum_{output_label}.pdf")
-pdf = matplotlib.backends.backend_pdf.PdfPages(sum_pdf_fpath)
+bp_pdf = matplotlib.backends.backend_pdf.PdfPages(bp_pdf_fpath)
+summary_pdf = matplotlib.backends.backend_pdf.PdfPages(sum_pdf_fpath)
+
 for proj in range(len(projects)):
 	project = projects[proj]
 	output_dir = os.path.join(home_dir, project, "output")
@@ -166,9 +169,10 @@ for proj in range(len(projects)):
 			# --------------------------------------------------------------------------
 			print(f"Finished recording accuracy. Heading towards boxplot creation for {philr_group} {project}.")
 			# --------------------------------------------------------------------------
+		else:
+			print(f"Algo table already made for {project} {philr_group}")
 		#Setup for building boxplots
 		result_df = pd.read_csv(result_fpath, sep=',', header=0)
-		print(result_df.head())
 		algos = list(set(result_df.loc[:,"model"]))
 		algos.sort()
 		print(algos)
@@ -182,138 +186,134 @@ for proj in range(len(projects)):
 		# f_header = ["feature", "f_mean","f_sd", "top_algo"]
 		# algo_table  = pd.DataFrame(columns = f_header)
 
-		# pdf = matplotlib.backends.backend_pdf.PdfPages(pdf_fpath)
-		# # sub_plot_counter = 0
-		# for meta_c in metadata_cats:
-		# 	meta_result_df = pd.DataFrame(result_df[result_df["metadata"] == meta_c])
-		# 	flat_num_only = pd.DataFrame(meta_result_df.iloc[:,5:]).to_numpy().flatten()
-		# 	f_mean = np.nanmean(flat_num_only)
-		# 	f_sd = np.std(flat_num_only)
-		# 	print("f_mean", str(f_mean), type(f_mean))
-		# 	f_sd = meta_result_df.iloc[:,5:].values.std()
-		# 	f_max = max(meta_result_df)
-		# 	f_min = min(meta_result_df)
-		# 	f_q1 = np.quantile(flat_num_only, .25)
-		# 	f_q2 = np.quantile(flat_num_only, .50)
-		# 	f_q3 = np.quantile(flat_num_only, .75)
-		# 	#for boxplot
-		# 	fig = plt.figure(figsize=(11,11))
-		# 	fig.suptitle(f"{project} PhILR weighting ML accuracy {meta_c} for {philr_group}.", fontsize=20)
-		# 	plt.subplots_adjust(bottom=0.8)
+		# sub_plot_counter = 0
+		for meta_c in metadata_cats:
+			meta_result_df = pd.DataFrame(result_df[result_df["metadata"] == meta_c])
+			flat_num_only = pd.DataFrame(meta_result_df.iloc[:,5:]).to_numpy().flatten()
+			f_mean = np.nanmean(flat_num_only)
+			f_sd = np.std(flat_num_only)
+			print("f_mean", str(f_mean), type(f_mean))
+			f_sd = meta_result_df.iloc[:,5:].values.std()
+			f_max = max(meta_result_df)
+			f_min = min(meta_result_df)
+			f_q1 = np.quantile(flat_num_only, .25)
+			f_q2 = np.quantile(flat_num_only, .50)
+			f_q3 = np.quantile(flat_num_only, .75)
+			#for boxplot
+			fig = plt.figure(figsize=(11,11))
+			fig.suptitle(f"{project} PhILR weighting ML accuracy {meta_c} for {philr_group}.", fontsize=20)
+			plt.subplots_adjust(bottom=0.8)
 
-		# 	# for algo table
-		# 	algo_table_line = [str(meta_c), str(f_mean), str(f_sd)]
-		# 	algo_means = dict()
-		# 	summary_table = dict()
+			# # for algo table
+			# algo_table_line = [str(meta_c), str(f_mean), str(f_sd)]
+			# algo_means = dict()
+			# summary_table = dict()
 
-		# 	if not pd.isna(f_mean):#don't proced if the data for the metadata is wonky
-		# 		fig_means = dict() #to hold data for summary boxplot figure
-		# 		for al in range(len(algos)):
-		# 			algo = algos[al]
-		# 			# print(algo)
-		# 			fig_df = pd.DataFrame(meta_result_df[meta_result_df["model"] == algo])
-		# 			# print(fig_df.head())
-		# 			plot_data = fig_df.iloc[:,5:].transpose()
-		# 			# print(f"df shape: {fig_df.shape[0]} {fig_df.shape[1]}")
-		# 			#side loop to get means of each
-		# 			fig_means[algo] = list(plot_data.mean(axis=0))
-		# 			#for boxplot
-		# 			ax = fig.add_subplot(num_rows,num_cols, al +1)
-		# 			ax.set_ylabel(scoring)
-		# 			ax.boxplot(plot_data)
-		# 			ax.title.set_text(f"{algo} by weighting scheme")
-		# 			ax.axhline(np.nanmean(plot_data), c="r", linestyle="dashed", label="Metadata mean")
-		# 			ax.axhline(f_mean, c="g", linestyle = ("-."), label = "Algorithm mean", marker = "_")
-		# 			ax.locator_params(axis='y', tight=True, nbins=4)
-		# 			new_labs = [f"{x}\n{y}" for x,y in zip(fig_df.loc[:,"ilr_weight"].values, fig_df.loc[:,"part_weight"].values)]
-		# 			# ax.set_xticklabels(fig_df.loc[:,"ilr_weight"].tolist(), rotation=90)
-		# 			ax.set_xticklabels(new_labs, rotation=90)
-		# 			ax.tick_params(axis='x', which='major', labelsize=6)
-		# 			ax.set_ylim([0.5, 1])#ax.set_ylim([f_mean - (1.5 * f_sd), 1])
-		# 			#for algo table
-		# 			algo_means[algo] = np.nanmean(plot_data) - f_mean
+			if not pd.isna(f_mean):#don't proced if the data for the metadata is wonky
+				fig_means = dict() #to hold data for summary boxplot figure
+				for al in range(len(algos)):
+					algo = algos[al]
+					# print(algo)
+					fig_df = pd.DataFrame(meta_result_df[meta_result_df["model"] == algo])
+					# print(fig_df.head())
+					plot_data = fig_df.iloc[:,5:].transpose()
+					# print(f"df shape: {fig_df.shape[0]} {fig_df.shape[1]}")
+					#side loop to get means of each
+					fig_means[algo] = list(plot_data.mean(axis=0))
+					#for boxplot
+					ax = fig.add_subplot(num_rows,num_cols, al +1)
+					ax.set_ylabel(scoring)
+					ax.boxplot(plot_data)
+					ax.title.set_text(f"{algo} by weighting scheme")
+					ax.axhline(np.nanmean(plot_data), c="r", linestyle="dashed", label="Metadata mean")
+					ax.axhline(f_mean, c="g", linestyle = ("-."), label = "Algorithm mean", marker = "_")
+					ax.locator_params(axis='y', tight=True, nbins=4)
+					new_labs = [f"{x}\n{y}" for x,y in zip(fig_df.loc[:,"ilr_weight"].values, fig_df.loc[:,"part_weight"].values)]
+					# ax.set_xticklabels(fig_df.loc[:,"ilr_weight"].tolist(), rotation=90)
+					ax.set_xticklabels(new_labs, rotation=90)
+					ax.tick_params(axis='x', which='major', labelsize=6)
+					ax.set_ylim([0.5, 1])#ax.set_ylim([f_mean - (1.5 * f_sd), 1])
+					# #for algo table
+					# algo_means[algo] = np.nanmean(plot_data) - f_mean
 
-		# 		ax = fig.add_subplot(num_rows,num_cols, al +2)
-		# 		ax.get_xaxis().set_visible(False)
-		# 		ax.get_yaxis().set_visible(False)
-		# 		ax.axhline(0, c="r", linestyle="dashed", label="Algorithm mean")
-		# 		ax.axhline(0, c="g", linestyle = ("-."), label = "Metadata mean", marker = "_")
-		# 		ax.legend(title="Legend", loc="center", framealpha=1, mode = "expand", markerscale=2)
+				ax = fig.add_subplot(num_rows,num_cols, al +2)
+				ax.get_xaxis().set_visible(False)
+				ax.get_yaxis().set_visible(False)
+				ax.axhline(0, c="r", linestyle="dashed", label="Algorithm mean")
+				ax.axhline(0, c="g", linestyle = ("-."), label = "Metadata mean", marker = "_")
+				ax.legend(title="Legend", loc="center", framealpha=1, mode = "expand", markerscale=2)
 
-		# 		#for boxplot
-		# 		fig.tight_layout()
-		# 		pdf.savefig( fig )
-		# 		print(pd.DataFrame.from_dict(fig_means))
-		# 		print(list(fig_means.keys()))
+				#for boxplot
+				fig.tight_layout()
+				bp_pdf.savefig( fig )
+				print(pd.DataFrame.from_dict(fig_means))
+				print(list(fig_means.keys()))
 
-		# 		#for algo table
-		# 		algo_means["top_algo"] = max(algo_means, key=algo_means.get)
-		# 		algo_means["feature"] = meta_c
-		# 		algo_means["f_mean"] = f_mean
-		# 		algo_means["f_sd"] = f_sd
-		# 		algo_table = algo_table.append(algo_means, ignore_index=True)
-
-		# print(f"Saving pdf for {philr_group}")
-		# pdf.close()
+				# #for algo table
+				# algo_means["top_algo"] = max(algo_means, key=algo_means.get)
+				# algo_means["feature"] = meta_c
+				# algo_means["f_mean"] = f_mean
+				# algo_means["f_sd"] = f_sd
+				# algo_table = algo_table.append(algo_means, ignore_index=True)
 
 		# algo_table = algo_table.reindex(columns=f_header)
 		# algo_table = algo_table.round(decimals = 3)
 		# print(f"Saving algo table for or {philr_group}")
 		# algo_table.to_csv(algo_table_fpath, index = False)
 
-		# # --------------------------------------------------------------------------
-		# print(f"Building another summary table to show best weights for each feature and algo or {philr_group}.")
-		# # --------------------------------------------------------------------------
-		# w_header = ["feature", "algo", "algo_mean", "algo_sd", "top_pw", "top_ilr"]
-		# # For each feature, for each algo, top_pw, top_ilr
-		# weight_dict = dict(
-		# 				feature = [],
-		# 				algo_name = [],
-		# 				algo_comb_sd = [],
-		# 				algo_comb_mean = [],
-		# 				algo_top_mean =[],
-		# 				top_pw = [],
-		# 				top_ilr = [])
+		# --------------------------------------------------------------------------
+		print(f"Building another summary table to show best weights for each feature and algo or {philr_group}.")
+		# --------------------------------------------------------------------------
+		w_header = ["feature", "algo", "algo_mean", "algo_sd", "top_pw", "top_ilr"]
+		# For each feature, for each algo, top_pw, top_ilr
+		weight_dict = dict(
+						feature = [],
+						algo_name = [],
+						algo_comb_sd = [],
+						algo_comb_mean = [],
+						algo_top_mean =[],
+						top_pw = [],
+						top_ilr = [])
 
-		# summary_fig_data = { "feature" : [], "algo" : [], "weigh_scheme": [], "mean":[]}
-		# for meta_c in metadata_cats:
-		# 	f_mean = np.nanmean(result_df.iloc[:,4:])
-		# 	meta_result_df = pd.DataFrame(result_df[result_df["metadata"] == meta_c])
-		# 	f_mean = np.nanmean(meta_result_df.iloc[:,5:])
-		# 	f_sd = np.std(meta_result_df.iloc[:,5:], ddof=1)
-		# 	algo_means = dict()
-		# 	if not pd.isna(f_mean) and f_mean != 0:#don't proced if the data for the metadata is wonky
-		# 		for al in range(len(algos)):
-		# 			algo = algos[al]
-		# 			# print(algo)
-		# 			fig_df = pd.DataFrame(meta_result_df[meta_result_df["model"] == algo])
-		# 			plot_data = fig_df.iloc[:,5:]
-		# 			algo_mean = np.nanmean(plot_data)
-		# 			all_means = fig_df.mean(axis=1)# find all means of "plot data"
-		# 			print("all means")
-		# 			print(len(all_means))
-		# 			top_mean = max(all_means)#pick pw and il from highest means
-		# 			al_sd = plot_data.values.std()
-		# 			meta_result_df.iloc[:,5:].values.std()
-		# 			for ind, my_mean in enumerate(all_means):
-		# 				if my_mean == top_mean:
-		# 					top_index = ind
-		# 					# print(ind, my_mean)
-		# 			my_pw = list(fig_df["part_weight"])[top_index]
-		# 			my_iw = list(fig_df["ilr_weight"])[top_index]
-		# 			weight_dict["feature"].append(meta_c)
-		# 			weight_dict["algo_name"].append(algo)
-		# 			weight_dict["algo_comb_mean"].append(algo_mean)
-		# 			weight_dict["algo_comb_sd"].append(al_sd)
-		# 			weight_dict["algo_top_mean"].append(top_mean)
-		# 			weight_dict["top_pw"].append(my_pw)
-		# 			weight_dict["top_ilr"].append(my_iw)
+		summary_fig_data = { "feature" : [], "algo" : [], "weigh_scheme": [], "mean":[]}
+		for meta_c in metadata_cats:
+			f_mean = np.nanmean(result_df.iloc[:,4:])
+			meta_result_df = pd.DataFrame(result_df[result_df["metadata"] == meta_c])
+			f_mean = np.nanmean(meta_result_df.iloc[:,5:])
+			f_sd = np.std(meta_result_df.iloc[:,5:], ddof=1)
+			algo_means = dict()
+			if not pd.isna(f_mean) and f_mean != 0:#don't proced if the data for the metadata is wonky
+				for al in range(len(algos)):
+					algo = algos[al]
+					# print(algo)
+					fig_df = pd.DataFrame(meta_result_df[meta_result_df["model"] == algo])
+					plot_data = fig_df.iloc[:,5:]
+					algo_mean = np.nanmean(plot_data)
+					all_means = fig_df.mean(axis=1)# find all means of "plot data"
+					print("all means")
+					print(len(all_means))
+					top_mean = max(all_means)#pick pw and il from highest means
+					al_sd = plot_data.values.std()
+					meta_result_df.iloc[:,5:].values.std()
+					for ind, my_mean in enumerate(all_means):
+						if my_mean == top_mean:
+							top_index = ind
+							# print(ind, my_mean)
+					my_pw = list(fig_df["part_weight"])[top_index]
+					my_iw = list(fig_df["ilr_weight"])[top_index]
+					weight_dict["feature"].append(meta_c)
+					weight_dict["algo_name"].append(algo)
+					weight_dict["algo_comb_mean"].append(algo_mean)
+					weight_dict["algo_comb_sd"].append(al_sd)
+					weight_dict["algo_top_mean"].append(top_mean)
+					weight_dict["top_pw"].append(my_pw)
+					weight_dict["top_ilr"].append(my_iw)
 
-		# weight_table = pd.DataFrame(weight_dict)
-		# weight_table.to_csv(weight_fpath, index = False)
-		# # --------------------------------------------------------------------------
-		# print(f"Summary table to be found at: {weight_fpath} for {philr_group}.")
-		# # --------------------------------------------------------------------------
+		weight_table = pd.DataFrame(weight_dict)
+		weight_table.to_csv(weight_fpath, index = False)
+		# --------------------------------------------------------------------------
+		print(f"Summary table to be found at: {weight_fpath} for {philr_group}.")
+		# --------------------------------------------------------------------------
 
 		# --------------------------------------------------------------------------
 		print(f"Building summary scatterplot for {philr_group}.")
@@ -338,7 +338,7 @@ for proj in range(len(projects)):
 				my_range = np.arange(round(f_mean - (1.5 * f_sd),1), round(f_mean + (1.5 * f_sd),1), round(y_tick_interval, 2))
 				sub_plot_counter += 1
 				if sub_plot_counter % max_plots_per_page == 0:
-					pdf.savefig( fig )
+					summary_pdf.savefig( fig )
 					fig = plt.figure(figsize=(11,12))
 					fig.suptitle(f"{project}, Algorithm by PhILR weighting {scoring} ")
 				for al in range(len(algos)):
@@ -383,11 +383,12 @@ for proj in range(len(projects)):
 		ax.get_yaxis().set_visible(False)
 		ax.legend(title="Algorithm", loc="center", framealpha=1, mode = "expand", markerscale=2)
 		fig.tight_layout()
-		pdf.savefig( fig )
+		summary_pdf.savefig( fig )
 # --------------------------------------------------------------------------
 print(f"Saved summary scatterplot to {sum_pdf_fpath}.")
 # --------------------------------------------------------------------------
-pdf.close()
+summary_pdf.close()
+bp_pdf.close()
 
 # --------------------------------------------------------------------------
 print(f"{__file__} complete!")
