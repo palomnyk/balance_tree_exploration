2853333c9c0a27cf5b7c3386d6c879b8d150a642
Aaron_Y <amyerke@uncc.edu>
2023-03-08 20:06:24 -0500

    adding final fig to this script

--- metastudies/scripts/mlm_metrics/all_sum_sklearn_ml_metrics_acc.py
+++ metastudies/scripts/mlm_metrics/all_sum_sklearn_ml_metrics_acc.py
@@ -15,6 +15,7 @@ print("Loading external libraries.")
 from cProfile import label
 import math
 import os, sys
+from statistics import mean
 from matplotlib import markers
 import numpy as np
 import pandas as pd
@@ -182,7 +183,6 @@ for proj in range(len(projects)):
 		num_cols = 2
 		num_rows = abs(-len(algos)//num_cols)
 		print(result_df.head())
-
 		# f_header = ["feature", "f_mean","f_sd", "top_algo"]
 		# algo_table  = pd.DataFrame(columns = f_header)
 
@@ -389,6 +389,85 @@ print(f"Saved summary scatterplot to {sum_pdf_fpath}.")
 # --------------------------------------------------------------------------
 summary_pdf.close()
 bp_pdf.close()
+"""
+To make plot of aggregated summary plot
+Combine result_df results for all philr combinations, ie: uniform vs uniform for each seperate MLA
+The results look something like this:
+
+metadata ilr_weight part_weight  model    split1    split2    split3    split4    split5    split6    split7    split8    split9   split10
+ETHNICITY    uniform     uniform     LR  0.690909  0.781818  0.654545  0.818182  0.685185  0.666667  0.703704  0.722222  0.796296  0.759259
+ETHNICITY    uniform     uniform    LDA  0.763636  0.745455  0.636364  0.818182  0.740741  0.685185  0.777778  0.759259  0.814815  0.814815
+"""
+# --------------------------------------------------------------------------
+print(f"Aggregating data for aggregated summary scatterplot.")
+# --------------------------------------------------------------------------
+# Nested dict for storing aggragated variables for final plot
+# keys will be {model}: {ilr_weight}_{part weight} : []
+# and added in summary aggregate plot section
+weights_models = {}
+
+for proj in range(len(projects)):
+	project = projects[proj]
+	output_dir = os.path.join(home_dir, project, "output")
+	for philr_group in philr_groups:
+		main_output_label = f"{output_label}_{philr_group}"
+
+		philr_dir = os.path.join(output_dir, "tables", philr_group)
+		if not os.path.exists(philr_dir):
+			print(f"""{philr_dir} does not exist.
+				Use lib/cml_scripts/mlm_metrics/PhILR_random_trees_and_counts_tables.R 
+				to create it.""")
+			sys.exit()
+		result_fpath = os.path.join(output_dir, "tables", f"{main_output_label}_{project}_{philr_group}_data.csv")
+		result_df = pd.read_csv(result_fpath, sep=',', header=0)
+		for line in range(result_df.shape[0]):
+			#Dict for storing aggragated variables for final plot
+			# Nested dict for storing aggragated variables for final plot
+			# keys will be {model}: [{ilr_weight}_{part weight}:[],]
+			my_model = result_df.loc[line, "model"]
+			my_part = result_df.loc[line, "part_weight"]
+			my_ilr = result_df.loc[line, "ilr_weight"]
+			my_data = list(result_df.iloc[line, 4:])
+			key_label = f"{my_part}_{my_ilr}"
+			if not my_model in weights_models.keys():
+				weights_models[f"{my_model}"] = {}
+			if key_label in weights_models[f"{my_model}"]:
+				weights_models[f"{my_model}"][f"{key_label}"].extend(my_data)
+			else:
+				weights_models[my_model][f"{key_label}"] = my_data
+
+agg_sum_pdf_fpath = os.path.join(home_dir, "metastudies", "output", f"agg_summary_mla_philr.pdf")
+pdf = matplotlib.backends.backend_pdf.PdfPages(agg_sum_pdf_fpath)
+fig = plt.figure(figsize=(7,6))
+fig.suptitle(f"Aggregated MLA by PhILR weighting scores")
+ax = fig.add_subplot()
+plt.subplots_adjust(bottom=0.3)
+ax.set_ylabel("Score")
+for num, model_key in enumerate(weights_models.keys()):
+	print(model_key)
+	weight_keys = sorted(weights_models[model_key].keys())
+	means = []
+	stds = []
+	labels = []
+	for weight_key in weight_keys:
+		my_data = weights_models[model_key][weight_key]
+		means.append(np.nanmean(weights_models[model_key][weight_key]))
+		stds.append(np.nanstd(weights_models[model_key][weight_key]))
+		labels.append(weight_key.replace("_", "\n"))
+	ax.plot(labels,
+					means,
+					label=model_key,
+					marker=model_symbols[num], 
+					linewidth=1, 
+					markersize=2)
+	# ax.set_title(f"", fontsize=10)
+	ax.set_xticks(ticks=range(0,len(means)), labels=labels, rotation=90)
+	ax.set_yticks(ticks=[0.55,0.65,0.75,0.85,0.95])
+	ax.tick_params(axis='x', which='major', labelsize=8)
+	ax.legend()
+
+pdf.savefig( fig )
+pdf.close()
 
 # --------------------------------------------------------------------------
 print(f"{__file__} complete!")
